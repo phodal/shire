f50db277fb0e4e18617fc2af8c1db053230a101c
Phodal Huang <h@phodal.com>
2024-06-01 13:41:25 +0800

    refactor(exec): refactor test service handling in RunShireCommand
    Refactor the handling of test service in the RunShireCommand for better clarity and functionality.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/exec/RunShireCommand.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/exec/RunShireCommand.kt
@@ -1,6 +1,9 @@
 package com.phodal.shirelang.compiler.exec
 
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.phodal.shirecore.provider.AutoTesting
 import com.phodal.shirelang.compiler.error.SHIRE_ERROR
 import com.phodal.shirelang.utils.lookupFile
 
@@ -15,12 +18,14 @@ class RunShireCommand(val myProject: Project, private val argument: String) : Sh
     override suspend fun doExecute(): String {
         val virtualFile = myProject.lookupFile(argument.trim()) ?: return "$SHIRE_ERROR: File not found: $argument"
         try {
-//            val psiFile: PsiFile =
-//                PsiManager.getInstance(myProject).findFile(virtualFile) ?: return "$SHIRE_ERROR: File not found: $argument"
-//            val testService =
-//                AutoTestService.context(psiFile) ?: return "$SHIRE_ERROR: No test service found for file: $argument"
-//            testService.runFile(myProject, virtualFile, null)
-            throw NotImplementedError()
+            val psiFile: PsiFile =
+                PsiManager.getInstance(myProject).findFile(virtualFile)
+                    ?: return "$SHIRE_ERROR: File not found: $argument"
+            val testService =
+                AutoTesting.context(psiFile) ?: return "$SHIRE_ERROR: No test service found for file: $argument"
+            testService.runFile(myProject, virtualFile, null)
+
+            return "Tests run successfully for file: $argument"
         } catch (e: Exception) {
             return "$SHIRE_ERROR: ${e.message}"
         }
