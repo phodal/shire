15a8b4c48bfe785bbd26be2b958c05eeec7ac700
Phodal Huang <h@phodal.com>
2024-06-04 16:09:35 +0800

    refactor(action): update ShireContextActionGroup implementation
    Updated ShireContextActionGroup implementation by adding ActionUpdateThread and modifying the update method.

--- src/main/kotlin/com/phodal/shire/action/context/ShireContextActionGroup.kt
+++ src/main/kotlin/com/phodal/shire/action/context/ShireContextActionGroup.kt
@@ -1,10 +1,17 @@
 package com.phodal.shire.action.context
 
 import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class ShireContextActionGroup : ActionGroup() {
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+//        e.presentation.isPopupGroup = getContextProviders().size > 1
+    }
+
     override fun getChildren(e: AnActionEvent?): Array<AnAction> {
         return arrayOf()
     }
