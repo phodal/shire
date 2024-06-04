62a52c24f1dba9341e569aff76a9cd92e0957ae7
Phodal Huang <h@phodal.com>
2024-06-04 21:22:27 +0800

    refactor(startup): move ShireActionStartupActivity to shirelang package
    Move ShireActionStartupActivity to the shirelang package and update references accordingly.

--- shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
@@ -1,18 +1,20 @@
-package com.phodal.shire.startup
+package com.phodal.shirelang
 
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.ProjectScope
-import com.phodal.shirelang.ShireFileType
 
 class ShireActionStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         // check all ShireLanguage
-//        val searchScope: GlobalSearchScope = ProjectScope.getProjectScope(project)
-//        val virtualFiles = FileTypeIndex.getFiles(ShireFileType.INSTANCE, searchScope)
-//
-//        println("Shire Files: ${virtualFiles.size}")
+        val searchScope: GlobalSearchScope = ProjectScope.getProjectScope(project)
+        smartReadAction(project) {
+            val virtualFiles = FileTypeIndex.getFiles(ShireFileType.INSTANCE, searchScope)
+
+            println("Shire Files: ${virtualFiles.size}")
+        }
     }
 }
