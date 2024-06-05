12f8d7a07f2b3f8b09ed03942f44c5f93ac12531
Phodal Huang <h@phodal.com>
2024-06-05 09:32:08 +0800

    refactor(shirelang): optimize file search in ShireActionStartupActivity
    Improve file search efficiency by using a more optimized method in ShireActionStartupActivity.

--- shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
@@ -1,8 +1,10 @@
 package com.phodal.shirelang
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.ProjectScope
@@ -12,9 +14,18 @@ class ShireActionStartupActivity : ProjectActivity {
         // check all ShireLanguage
         val searchScope: GlobalSearchScope = ProjectScope.getProjectScope(project)
         smartReadAction(project) {
-            val virtualFiles = FileTypeIndex.getFiles(ShireFileType.INSTANCE, searchScope)
-
-            println("Shire Files: ${virtualFiles.size}")
+            getEditorConfigFiles(project).forEach { _ ->
+                // do something
+            }
         }
     }
+
+    fun getEditorConfigFiles(project: Project): Collection<VirtualFile> {
+        // Not allowed during indexing to prevent reentrant indexing (IDEA-277028)
+        ApplicationManager.getApplication().assertReadAccessAllowed()
+        val allScope = GlobalSearchScope.allScope(project)
+        val filesScope = GlobalSearchScope.getScopeRestrictedByFileTypes(allScope, ShireFileType.INSTANCE)
+        return FileTypeIndex.getFiles(ShireFileType.INSTANCE, filesScope)
+    }
+
 }
