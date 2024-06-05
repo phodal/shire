62891a0d0c5ffc8888515ff2471fdcab41c6b930
Phodal Huang <h@phodal.com>
2024-06-05 09:38:44 +0800

    feat(index): add ShireIdentifierIndex for file content
    Add ShireIdentifierIndex to index Shire file content for identification.

---
+++ shirelang/src/main/kotlin/com/phodal/shirelang/index/ShireIdentifierIndex.kt
@@ -0,0 +1,53 @@
+package com.phodal.shirelang.index
+
+import com.intellij.psi.PsiElement
+import com.intellij.util.indexing.DataIndexer
+import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter
+import com.intellij.util.indexing.FileBasedIndexExtension
+import com.intellij.util.indexing.FileContent
+import com.intellij.util.indexing.ID
+import com.intellij.util.io.DataExternalizer
+import com.intellij.util.io.EnumeratorStringDescriptor
+import com.intellij.util.io.KeyDescriptor
+import com.phodal.shirelang.ShireFileType
+import com.phodal.shirelang.psi.ShireFrontMatterEntry
+import com.phodal.shirelang.psi.ShireVisitor
+import java.io.DataInput
+import java.io.DataOutput
+
+internal val SHIRE_CONFIG_IDENTIFIER_INDEX_ID = ID.create<String, Int>("shire.index.name")
+
+internal val isIndexing = ThreadLocal<Boolean>()
+
+// todo: update for content
+class ShireIdentifierIndex: FileBasedIndexExtension<String, Int>() {
+    override fun getValueExternalizer() = object : DataExternalizer<Int> {
+        override fun save(out: DataOutput, value: Int) = out.writeInt(value)
+        override fun read(`in`: DataInput) = `in`.readInt()
+    }
+
+    override fun getIndexer() = DataIndexer<String, Int, FileContent> {
+        val result = mutableMapOf<String, Int>()
+        val visitor = object : ShireVisitor() {
+//            override fun visitElement(element: PsiElement) {
+//                if (element is ShireFrontMatterEntry) {
+//                    result[element.text] = element.textOffset
+//                }
+//                super.visitElement(element)
+//            }
+        }
+
+        isIndexing.set(true)
+        it.psiFile.accept(visitor)
+        isIndexing.set(false)
+        result
+    }
+
+    override fun getName() = SHIRE_CONFIG_IDENTIFIER_INDEX_ID
+    override fun getVersion() = 1
+    override fun dependsOnFileContent() = true
+    override fun getInputFilter() = inputFilter
+    override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
+
+    private val inputFilter = DefaultFileTypeSpecificInputFilter(ShireFileType.INSTANCE)
+}
