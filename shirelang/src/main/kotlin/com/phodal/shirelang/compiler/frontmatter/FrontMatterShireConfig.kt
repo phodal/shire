fa8993189330f570e2726fbef2ace279e894c725
Phodal Huang <h@phodal.com>
2024-06-06 14:53:00 +0800

    refactor(compiler): improve FrontMatterShireConfig structure
    - Refactored FrontMatterShireConfig to enhance clarity and organization.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/frontmatter/FrontMatterShireConfig.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/frontmatter/FrontMatterShireConfig.kt
@@ -10,13 +10,38 @@ sealed class ElementStrategy {
     object DEFAULT : ElementStrategy()
 }
 
+/**
+ * - Normal: the action is a normal action
+ * - Flow: each action can be a task in a flow, which will build a DAG
+ */
 data class FrontMatterShireConfig(
     val name: String,
     val description: String,
+    /**
+     * The output of the action can be a file, a string, etc.
+     */
     val interaction: InteractionType,
+    /**
+     * The location of the action, can [ShireActionLocation]
+     */
     val actionLocation: ShireActionLocation,
+    /**
+     * The data of the action.
+     */
     val data: Map<String, FrontMatterType> = mutableMapOf(),
+    /**
+     * The strategy to select the element to apply the action.
+     * If not selected text, will according the element position to select the element block.
+     * For example, if cursor in a function, select the function block.
+     */
     val elementStrategy: ElementStrategy = ElementStrategy.DEFAULT,
+    /**
+     * The list of actions that this action depends on.
+     * We use it for Directed Acyclic Graph (DAG) to represent dependencies between actions.
+     *
+     * todo: apply isApplicable for actions that do not depend on any tasks.
+     */
+    val dependsOn: List<String> = emptyList()
 ) {
     companion object {
         fun from(fm: MutableMap<String, FrontMatterType>): FrontMatterShireConfig? {
