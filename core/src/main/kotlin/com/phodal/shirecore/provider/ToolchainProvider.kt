6a6d1d8a679e15694618b5043311714027dda14d
Phodal Huang <h@phodal.com>
2024-06-07 22:50:51 +0800

    feat(core): add ToolchainProvider extension point
    Add ToolchainProvider extension point for providing toolchain functionality in the core module.

---
+++ core/src/main/kotlin/com/phodal/shirecore/provider/ToolchainProvider.kt
@@ -0,0 +1,52 @@
+package com.phodal.shirecore.provider
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import kotlin.reflect.KClass
+
+class ToolchainContextItem(
+    val clazz: KClass<*>,
+    var text: String
+)
+
+data class ToolchainPrepareContext(
+    val sourceFile: PsiFile?,
+    val extraItems: List<ToolchainContextItem> = emptyList(),
+    val element: PsiElement?
+)
+
+interface ToolchainProvider {
+    fun isApplicable(project: Project, creationContext: ToolchainPrepareContext): Boolean
+
+    @RequiresBackgroundThread
+    suspend fun collect(project: Project, creationContext: ToolchainPrepareContext): List<ToolchainContextItem>
+
+    companion object {
+        private val EP_NAME = ExtensionPointName<ToolchainProvider>("com.phodal.shireToolchainProvider")
+
+        suspend fun gatherToolchainContextItems(
+            project: Project,
+            toolchainPrepareContext: ToolchainPrepareContext,
+        ): List<ToolchainContextItem> {
+            val elements = mutableListOf<ToolchainContextItem>()
+
+            val chatContextProviders = EP_NAME.extensionList
+            for (provider in chatContextProviders) {
+                try {
+                    val applicable = provider.isApplicable(project, toolchainPrepareContext)
+                    if (applicable) {
+                        elements.addAll(provider.collect(project, toolchainPrepareContext))
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            elements.addAll(toolchainPrepareContext.extraItems)
+            return elements.distinctBy { it.text }
+        }
+    }
+}
