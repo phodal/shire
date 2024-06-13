50ce6aa3d3a805472f062f7fa7a9815887ad4598
Phodal Huang <h@phodal.com>
2024-06-13 16:55:23 +0800

    refactor(actions): refactor WhenConditionValidator to use PsiVariables
    Refactor WhenConditionValidator to use PsiVariables for better organization and readability. Added PsiVariables class with necessary properties and methods.

--- shirelang/src/main/kotlin/com/phodal/shirelang/actions/validator/WhenConditionValidator.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/actions/validator/WhenConditionValidator.kt
@@ -12,33 +12,53 @@ import com.phodal.shirelang.compiler.hobbit.Statement
  * - fileExtension
  * - fileContent
  */
+data class PsiVariables(
+    val filePath: String,
+    val fileName: String,
+    val fileType: String,
+    val fileExtension: String,
+    val fileContent: String,
+) {
+    companion object {
+        fun fromPsiFile(file: PsiFile): PsiVariables {
+            return PsiVariables(
+                file.virtualFile.path,
+                file.name,
+                file.fileType.name,
+                file.virtualFile.extension ?: "",
+                file.text
+            )
+        }
+
+        fun completionProvider(): Map<String, String> {
+            return mapOf(
+                "filePath" to "The path of the file",
+                "fileName" to "The name of the file",
+                "fileType" to "The type of the file",
+                "fileExtension" to "The extension of the file",
+                "fileContent" to "The content of the file"
+            )
+        }
+    }
+}
+
 class WhenConditionValidator(
-    private val filePath: String,
-    private val fileName: String,
-    private val fileType: String,
-    private val fileExtension: String,
-    private val fileContent: String
+    private val psiVariables: PsiVariables
 ) {
 
     fun variables(): Map<String, String> {
         return mapOf(
-            "filePath" to filePath,
-            "fileName" to fileName,
-            "fileType" to fileType,
-            "fileExtension" to fileExtension,
-            "fileContent" to fileContent
+            "filePath" to psiVariables.filePath,
+            "fileName" to psiVariables.fileName,
+            "fileType" to psiVariables.fileType,
+            "fileExtension" to psiVariables.fileExtension,
+            "fileContent" to psiVariables.fileContent
         )
     }
 
     companion object {
         fun build(file: PsiFile): WhenConditionValidator {
-            return WhenConditionValidator(
-                file.virtualFile.path,
-                file.name,
-                file.fileType.name,
-                file.fileType.defaultExtension,
-                file.text
-            )
+            return WhenConditionValidator(PsiVariables.fromPsiFile(file))
         }
 
         fun isAvailable(conditions: FrontMatterType.Expression, file: PsiFile): Boolean {
