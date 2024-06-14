4eb84e425b8fb757bd223eb4c75814933c943d56
Phodal Huang <h@phodal.com>
2024-06-14 15:10:38 +0800

    refactor(provider): update CodeStructVariable enum and documentation #14
    Updated the CodeStructVariable enum with more descriptive values and improved documentation for each variable.

--- core/src/main/kotlin/com/phodal/shirecore/provider/CodeStructVariableProvider.kt
+++ core/src/main/kotlin/com/phodal/shirecore/provider/CodeStructVariableProvider.kt
@@ -6,13 +6,6 @@ import com.intellij.lang.LanguageExtension
 /**
  * Resolve variables for code struct generation.
  * This is used to provide the variables that are used in the code struct generation.
- *
- * - currentClass
- * - relatedClasses
- * - similarTestCase
- * - imports
- * - isNewFile
- * - targetTestFileName
  */
 interface CodeStructVariableProvider {
     companion object {
@@ -23,4 +16,43 @@ interface CodeStructVariableProvider {
             return languageExtension.forLanguage(language)
         }
     }
+}
+
+/**
+ * Code Structure variables
+ * - currentClass: The current class for which the code struct is being generated.
+ * - relatedClasses: Any related classes that are relevant to the code struct generation.
+ * - similarTestCase: Any similar test cases that can be used as reference for the code struct generation.
+ * - imports: The necessary imports required for the code struct generation.
+ * - isNewFile: A flag indicating whether the code struct is being generated in a new file.
+ * - targetTestFileName: The name of the target test file where the code struct will be generated.
+ */
+enum class CodeStructVariable {
+    /**
+     * return PsiNameIdentifierOwner of the current class, so we can get the class name
+     */
+    CURRENT_CLASS,
+    /**
+     * Input and output of PsiElement and PsiFile
+     */
+    RELATED_CLASSES,
+    /**
+     * Use TfIDF to search code for similar test case
+     */
+    SIMILAR_TEST_CASE,
+
+    /**
+     * Import statements for the code struct
+     */
+    IMPORTS,
+
+    /**
+     * Flag indicating whether the code struct is being generated in a new file
+     */
+    IS_NEW_FILE,
+
+    /**
+     * The name of the target test file where the code struct will be generated
+     */
+    TARGET_TEST_FILE_NAME
 }
