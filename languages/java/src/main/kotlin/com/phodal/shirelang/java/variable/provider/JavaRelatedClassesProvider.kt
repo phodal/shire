295a8d2050d4398c5c4875f6691aa31f96bf22e7
Phodal Huang <h@phodal.com>
2024-06-17 09:13:28 +0800

    refactor(java): optimize JavaRelatedClassesProvider methods
    Refactor `lookup` and `cleanUp` methods in `JavaRelatedClassesProvider` for better readability and performance.

--- languages/java/src/main/kotlin/com/phodal/shirelang/java/variable/provider/JavaRelatedClassesProvider.kt
+++ languages/java/src/main/kotlin/com/phodal/shirelang/java/variable/provider/JavaRelatedClassesProvider.kt
@@ -7,25 +7,46 @@ import com.intellij.psi.util.PsiUtil
 
 class JavaRelatedClassesProvider {
     fun lookup(method: PsiMethod): List<PsiClass> {
-        val relatedClasses = findRelatedClasses(method)
-        val cleanedUpClasses = relatedClasses.flatMap { findSuperClasses(it) }.map { cleanUp(it) }
-        return cleanedUpClasses.toList()
+        return findRelatedClasses(method)
+            .flatMap { findSuperClasses(it) }
+            .map { cleanUp(it) }
+            .toList()
     }
 
+    /**
+     * Finds related classes to the given PsiMethod by analyzing its parameters, return type, and generic types.
+     *
+     * @param method the PsiMethod for which related classes need to be found
+     * @return a list of PsiClass instances that are related to the given PsiMethod, filtered to include only classes that are part of the project content
+     */
+    private fun findRelatedClasses(method: PsiMethod): List<PsiClass> {
+        val parameters = method.parameterList.parameters
+        val parameterTypes = parameters.map { it.type }
+        val genericTypes = parameters.flatMap { (it.type as? PsiClassType)?.parameters?.toList() ?: emptyList() }
+        val mentionedTypes = parameterTypes + method.returnType + genericTypes
+        val relatedTypes = mentionedTypes.filterIsInstance<PsiClassType>()
+        val resolvedClasses = relatedTypes.mapNotNull { it.resolve() }
+        val projectContentClasses = resolvedClasses.filter { isProjectContent(it) }
+        return projectContentClasses.toList()
+    }
+
+    /**
+     * Cleans up a given PsiClass by removing unnecessary elements such as method bodies, method comments, and any other removable members.
+     *
+     * @param psiClass the PsiClass to be cleaned up
+     * @return a new PsiClass with the unnecessary elements removed
+     */
     fun cleanUp(psiClass: PsiClass): PsiClass {
         val psiElement = psiClass.copy() as PsiClass
         psiElement.containingFile.setName(psiClass.containingFile.name)
+
         val members = PsiTreeUtil.findChildrenOfType(psiElement, PsiMember::class.java)
 
-        for (element in members) {
-            if (element is PsiMethod) {
-                element.body?.delete()
-                element.docComment?.delete()
-            }
+        members.filterIsInstance<PsiMethod>().forEach {
+            it.body?.delete()
+            it.docComment?.delete()
         }
-
-        val removableMembers = members.filter { canBeRemoved(it) }
-        removableMembers.forEach { it.delete() }
+        members.filter { canBeRemoved(it) }.forEach { it.delete() }
 
         psiElement.docComment?.delete()
         return psiElement
@@ -47,17 +68,6 @@ class JavaRelatedClassesProvider {
     private fun canBeRemoved(member: PsiMember): Boolean {
         if (member.modifierList?.hasModifierProperty("public") == true) return false
         return member.annotations.isEmpty()
-    }
-
-    private fun findRelatedClasses(method: PsiMethod): List<PsiClass> {
-        val parameters = method.parameterList.parameters
-        val parameterTypes = parameters.map { it.type }
-        val genericTypes = parameters.flatMap { (it.type as? PsiClassType)?.parameters?.toList() ?: emptyList() }
-        val mentionedTypes = parameterTypes + method.returnType + genericTypes
-        val relatedTypes = mentionedTypes.filterIsInstance<PsiClassType>()
-        val resolvedClasses = relatedTypes.mapNotNull { it.resolve() }
-        val projectContentClasses = resolvedClasses.filter { isProjectContent(it) }
-        return projectContentClasses.toList()
     }
 
     private fun isProjectContent(element: PsiElement): Boolean {
