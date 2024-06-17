ce101d480fe7a86b957d1d72b4ea516c23c5d7fe
Phodal Huang <h@phodal.com>
2024-06-17 21:51:13 +0800

    refactor(core): rename and enhance FormatableElement class #16
    Renamed `NamedElementContext` to `FormatableElement` and added a `format()` method. This method formats the named element context into a string representation, which varies depending on the context. Examples include directory structure, file structure, UML, method signature, and variable declaration.

--- core/src/main/kotlin/com/phodal/shirecore/codemodel/base/FormatableElement.kt
+++ core/src/main/kotlin/com/phodal/shirecore/codemodel/base/FormatableElement.kt
@@ -3,13 +3,23 @@ package com.phodal.shirecore.codemodel.base
 import com.intellij.psi.PsiElement
 
 /**
- * The `NamedElementContext` class represents a context for a named element in Kotlin language.
- * It is an abstract class that provides information about the root element, text, and name of the element.
+ * The `FormatableElement` is an abstract class that represents a formatable element in a given context.
+ * It provides information about the root element, text, and name of the element.
  *
  * @property root The root element of the context.
  * @property text The text representation of the context.
  * @property name The name of the element in the context.
  *
+ * This class has a method `format()` which formats the named element context into a string representation.
+ * The format of the string representation varies depending on the context.
+ * For instance:
+ * In the context of `com.phodal.shirecore.codemodel.DirectoryStructure`, the formatted string representation will be like a directory structure.
+ * In the context of `com.phodal.shirecore.codemodel.FileStructure`, the formatted string representation will be like a file structure.
+ * In the context of `com.phodal.shirecore.codemodel.ClassStructure`, the formatted string representation will be like UML.
+ * In the context of `com.phodal.shirecore.codemodel.MethodStructure`, the formatted string representation will be just the method signature.
+ * In the context of `com.phodal.shirecore.codemodel.VariableStructure`, the formatted string representation will be like a variable declaration.
+ *
+ * @return The formatted string representation of the named element context.
  */
 abstract class FormatableElement(open val root: PsiElement, open val text: String?, open val name: String?) {
     /**
@@ -19,7 +29,6 @@ abstract class FormatableElement(open val root: PsiElement, open val text: Strin
      * 'package: cc.unitmesh.untitled.demo.controller.UserController
      * '@RestController, @RequestMapping("/user")
      * class UserController {
-     *
      *   + @GetMapping     public UserDTO getUsers()
      * }
      * ```
