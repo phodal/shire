411ebf4840152624f15a6800478bcf5715fdbf74
Phodal Huang <h@phodal.com>
2024-06-18 14:31:08 +0800

    feat(runner): enhance data fetching methods in SystemInfoVariable
    Refactor the data fetching methods in SystemInfoVariable.kt to fetch more detailed IDE information and simplify the code structure. The methods for fetching timezone and locale data have been streamlined, and the overall data resolution method has been simplified for better readability and efficiency.

--- shirelang/src/main/kotlin/com/phodal/shirelang/run/runner/SystemInfoVariable.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/run/runner/SystemInfoVariable.kt
@@ -1,5 +1,6 @@
 package com.phodal.shirelang.run.runner
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.util.PlatformUtils
 import java.util.*
 
@@ -12,7 +13,7 @@ import java.util.*
  */
 class SystemInfoVariable {
     companion object {
-        fun fetchOsData(): Map<String, Any> {
+        private fun fetchOsData(): Map<String, Any> {
             return mapOf(
                 "name" to System.getProperty("os.name"),
                 "version" to System.getProperty("os.version"),
@@ -20,32 +21,33 @@ class SystemInfoVariable {
             )
         }
 
-        fun fetchIDEData(): Map<String, Any> {
-            val ideInfo = mutableMapOf<String, Any>()
-            // Replace these with actual methods to get IDE information
-            ideInfo["name"] = System.getProperty(PlatformUtils.PLATFORM_PREFIX_KEY, "idea")
-            return ideInfo
+        /**
+         * Get data from [com.intellij.util.PlatformUtils]
+         */
+        private fun fetchIDEData(): Map<String, Any> {
+            val buildNumber = ApplicationInfo.getInstance().build
+            return mapOf(
+                "name" to System.getProperty("idea.platform.prefix", "idea"),
+                "version" to buildNumber.asString(),
+                "code" to buildNumber.productCode
+            )
         }
 
-        fun getTimezoneInfo(): Map<String, Any> {
-            val timezoneInfo = mutableMapOf<String, Any>()
-            timezoneInfo["timezone"] = TimeZone.getDefault().displayName
-            return timezoneInfo
+        private fun fetchTimezoneData(): Map<String, Any> {
+            return mapOf("timezone" to TimeZone.getDefault().displayName)
         }
 
-        fun getLocaleInfo(): Map<String, Any> {
-            val localeInfo = mutableMapOf<String, Any>()
-            localeInfo["locale"] = Locale.getDefault().toString()
-            return localeInfo
+        private fun fetchLocaleData(): Map<String, Any> {
+            return mapOf("locale" to Locale.getDefault().toString())
         }
 
         fun resolve(): Map<out String, Any> {
-            val results = mutableMapOf<String, Any>()
-            results.putAll(mapOf("os" to fetchOsData()))
-            results.putAll(mapOf("ide" to fetchIDEData()))
-            results.putAll(mapOf("timezone" to getTimezoneInfo()))
-            results.putAll(mapOf("locale" to getLocaleInfo()))
-            return results
+            return mapOf(
+                "os" to fetchOsData(),
+                "ide" to fetchIDEData(),
+                "timezone" to fetchTimezoneData(),
+                "locale" to fetchLocaleData()
+            )
         }
     }
 }
