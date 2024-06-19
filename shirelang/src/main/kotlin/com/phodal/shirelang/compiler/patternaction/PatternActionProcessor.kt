a773aff544ca25e66f12ed7b1610fce2ec78abdc
Phodal Huang <h@phodal.com>
2024-06-19 16:36:48 +0800

    refactor(patternaction): enhance transformation execution logic #18
    Improve the execution logic of transformations in PatternActionProcessor. The transformations are now applied in the order they are defined in the PatternActionTransform. The input is converted to a String before applying the transformations. The result of each transformation is used as the input for the next transformation. The executeCatFunc method is now called separately for Cat transformations.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/patternaction/PatternActionProcessor.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/patternaction/PatternActionProcessor.kt
@@ -19,6 +19,7 @@ class PatternActionProcessor(val myProject: Project, val editor: Editor, val hol
      */
     fun execute(actionTransform: PatternActionTransform): String {
         var input: Any = ""
+        // todo: update rules for input type
         if (actionTransform.pattern.isNotBlank()) {
             input = PatternSearcher.findFilesByRegex(myProject, actionTransform.pattern)
                 .map { it.path }
@@ -29,73 +30,82 @@ class PatternActionProcessor(val myProject: Project, val editor: Editor, val hol
     }
 
     /**
-     * Since we use `class Cat(vararg val paths: String)`, If input is [Collection] should use [Array]
+     * This method is used to execute a series of transformations on the input based on the provided PatternActionTransform.
+     * The transformations are applied in the order they are defined in the PatternActionTransform.
+     * The input can be of any type, but the transformations are applied as if the input is a String.
+     * If the input is not a String, it will be converted to a String before applying the transformations.
+     * The result of each transformation is used as the input for the next transformation.
+     * If the transformation is a Cat, the executeCatFunc method is called with the action and the original input.
+     * The result of the last transformation is returned as a String.
+     *
+     * @param transform The PatternActionTransform that defines the transformations to be applied.
+     * @param input The input on which the transformations are to be applied.
+     * @return The result of applying the transformations to the input as a String.
      */
     fun execute(transform: PatternActionTransform, input: Any): String {
         var result = input
         transform.patternActionFuncs.forEach { action ->
-            when (action) {
+            result = when (action) {
                 is PatternActionFunc.Prompt -> {
-                    result = action.message
+                    action.message
                 }
 
                 is PatternActionFunc.Grep -> {
-                    result = (result as String).split("\n").filter { line -> action.patterns.any { line.contains(it) } }
+                    (result as String).split("\n")
+                        .filter { line -> action.patterns.any { line.contains(it) } }
                         .joinToString("\n")
                 }
 
                 is PatternActionFunc.Sed -> {
-                    result = (result as String).replace(action.pattern.toRegex(), action.replacements)
+                    (result as String)
+                        .replace(action.pattern.toRegex(), action.replacements)
                 }
 
                 is PatternActionFunc.Sort -> {
-                    result = (result as String).split("\n").sorted().joinToString("\n")
+                    (result as String).split("\n")
+                        .sorted().joinToString("\n")
                 }
 
                 is PatternActionFunc.Uniq -> {
-                    result = (result as String).split("\n").distinct().joinToString("\n")
+                    (result as String).split("\n").distinct().joinToString("\n")
                 }
 
                 is PatternActionFunc.Head -> {
-                    result = (result as String).split("\n").take(action.number.toInt()).joinToString("\n")
+                    (result as String).split("\n").take(action.number.toInt()).joinToString("\n")
                 }
 
                 is PatternActionFunc.Tail -> {
-                    result = (result as String).split("\n").takeLast(action.number.toInt()).joinToString("\n")
+                    (result as String).split("\n").takeLast(action.number.toInt()).joinToString("\n")
                 }
 
                 is PatternActionFunc.Cat -> {
-                    val baseDir = myProject.guessProjectDir()!!
-                    var paths = action.paths
-                    if (action.paths.isEmpty()) {
-                        paths = input as Array<String>
-                    }
-
-                    var absolutePaths: List<VirtualFile> = paths.mapNotNull {
-                        baseDir.findFile(it)
-                    }
-                    if (absolutePaths.isEmpty()) {
-                        absolutePaths = paths.mapNotNull {
-                            LocalFileSystem.getInstance().findFileByIoFile(File(it))
-                        }
-                    }
-
-                    val content = absolutePaths.joinToString("\n") { it.readText() }
-
-                    result = content
+                    executeCatFunc(action, input)
                 }
 
                 is PatternActionFunc.Print -> {
-                    result = action.texts.joinToString("\n")
+                    action.texts.joinToString("\n")
                 }
 
                 is PatternActionFunc.Xargs -> {
-                    result = action.variables
+                    action.variables
                 }
             }
         }
 
         return result.toString()
+    }
+
+    private fun executeCatFunc(action: PatternActionFunc.Cat, input: Any): String {
+        val baseDir = myProject.guessProjectDir()!!
+        var paths = action.paths
+        if (action.paths.isEmpty()) {
+            paths = input as Array<String>
+        }
+        val absolutePaths: List<VirtualFile> = paths.mapNotNull {
+            baseDir.findFile(it) ?: LocalFileSystem.getInstance().findFileByIoFile(File(it))
+        }
+
+        return absolutePaths.joinToString("\n") { it.readText() }
     }
 
 }
