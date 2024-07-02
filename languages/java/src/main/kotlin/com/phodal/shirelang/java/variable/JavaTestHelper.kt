8548ec88d5008bc7a6c61da86e803d0f10d1cecf
Phodal Huang <h@phodal.com>
2024-07-02 16:41:10 +0800

    refactor(java): refactor findSimilarTestCases in JavaTestHelper.kt for better readability and performance

--- languages/java/src/main/kotlin/com/phodal/shirelang/java/variable/JavaTestHelper.kt
+++ languages/java/src/main/kotlin/com/phodal/shirelang/java/variable/JavaTestHelper.kt
@@ -6,8 +6,9 @@ import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.PsiShortNamesCache
 import com.intellij.psi.search.searches.ClassInheritorsSearch
-import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.*
 import com.phodal.shirecore.search.CodeNamingTokenizer
 import com.phodal.shirecore.search.TfIdf
 
@@ -39,17 +40,14 @@ object JavaTestHelper {
     }
 
     fun findSimilarTestCases(psiElement: PsiElement, minScore: Double = 1.0): List<PsiMethod> {
+        val project = psiElement.project
         val psiMethod = psiElement as? PsiMethod ?: return emptyList()
         val methodName = psiMethod.name
 
-        val containingDirectory = psiElement.containingFile.containingDirectory
-        val allTestMethods = containingDirectory.files
-            .asSequence()
-            .filter { it.name.contains("Test") && it is PsiJavaFile }
-            .flatMap { (it as PsiJavaFile).classes.asSequence() }
-            .flatMap { it.methods.asSequence() }
-            .toList()
+        // 使用缓存机制获取所有测试方法
+        val allTestMethods = getAllTestMethods(project)
 
+        // 使用 TfIdf 进行相似度计算
         val tfIdf = TfIdf<String, List<PsiMethod>>()
         tfIdf.setTokenizer(CodeNamingTokenizer())
 
@@ -60,4 +58,27 @@ object JavaTestHelper {
                 if (measure > minScore) allTestMethods[index] else null
             }
     }
-}
+
+    fun getAllTestMethods(project: Project): List<PsiMethod> {
+        val cachedValue: CachedValue<List<PsiMethod>> = CachedValuesManager.getManager(project).createCachedValue {
+            val testMethods = mutableListOf<PsiMethod>()
+            val scope = GlobalSearchScope.projectScope(project)
+
+            // 获取所有文件名中包含 'Test' 的类
+            PsiShortNamesCache.getInstance(project).getAllClassNames()
+                .filter { it.contains("Test") }
+                .forEach { className ->
+                    // 获取所有匹配的类
+                    PsiShortNamesCache.getInstance(project).getClassesByName(className, scope)
+                        .filter { it.containingFile.name.endsWith("Test.java") }
+                        .forEach { psiClass ->
+                            // 收集所有方法
+                            testMethods.addAll(psiClass.methods)
+                        }
+                }
+
+            CachedValueProvider.Result.create(testMethods, PsiModificationTracker.MODIFICATION_COUNT)
+        }
+
+        return cachedValue.value
+    }}
