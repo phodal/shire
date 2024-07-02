6448aa154ccc9a6e02213cf664dc7d6aa84aa05d
Phodal Huang <h@phodal.com>
2024-07-02 11:13:27 +0800

    feat(shirelang): update line marker provider for ShirePsiExpr
    Update the ShirePsiExprLineMarkerProvider to handle ShireCaseBody and ShireActionBody. Remove unused import and refactor the function to improve readability and maintainability.

--- shirelang/src/main/kotlin/com/phodal/shirelang/run/ShirePsiExprLineMarkerProvider.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/run/ShirePsiExprLineMarkerProvider.kt
@@ -2,27 +2,39 @@ package com.phodal.shirelang.run
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
-import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT
 import com.intellij.psi.PsiElement
 import com.phodal.shirelang.ShireIcons
+import com.phodal.shirelang.psi.ShireActionBody
+import com.phodal.shirelang.psi.ShireCaseBody
 import com.phodal.shirelang.psi.ShireFrontMatterEntry
+import com.phodal.shirelang.psi.ShireQueryStatement
 
 class ShirePsiExprLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         if (element !is ShireFrontMatterEntry) return null
-        if (element.functionStatement == null) return null
 
         val key = element.frontMatterKey!!.firstChild
 
-        val functionBody = element.functionStatement?.functionBody ?: return null
-        when {
-            functionBody.queryStatement != null -> {
+        val patternAction = element.patternAction
+        if (patternAction != null) {
+            val firstExpr = patternAction.actionBlock.actionBody.actionExprList.firstOrNull()
+            when (firstExpr?.firstChild) {
+                is ShireCaseBody -> {
+                    return LineMarkerInfo(key, key.textRange, ShireIcons.Case, null, null, LEFT)
+                    { "" }
+                }
+            }
+        }
+
+
+        when (element.functionStatement?.functionBody?.firstChild) {
+            is ShireQueryStatement -> {
                 return LineMarkerInfo(key, key.textRange, ShireIcons.PsiExpr, null, null, LEFT)
                 { "" }
             }
 
-            functionBody.actionBody != null -> {
+            is ShireActionBody -> {
                 return LineMarkerInfo(key, key.textRange, ShireIcons.Pipeline, null, null, LEFT)
                 { "" }
             }
