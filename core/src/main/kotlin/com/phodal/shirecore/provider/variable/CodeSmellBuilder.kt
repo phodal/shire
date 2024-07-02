3747e08ab52203e09c2e2883fa6c8df7bf726665
Phodal Huang <h@phodal.com>
2024-07-02 13:47:06 +0800

    refactor(CodeSmellBuilder): remove unused canonical name extraction method #29

--- core/src/main/kotlin/com/phodal/shirecore/provider/variable/CodeSmellBuilder.kt
+++ core/src/main/kotlin/com/phodal/shirecore/provider/variable/CodeSmellBuilder.kt
@@ -5,23 +5,8 @@ import com.intellij.lang.LanguageCommenters
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.phodal.shirecore.provider.psi.PsiElementDataBuilder
 
 object CodeSmellBuilder  {
-
-    private val CANONICAL_NAME_REGEX_PATTERN = Regex("""\(([a-zA-Z]+(\.[a-zA-Z]+)+(\.[a-zA-Z0-9_]+))\)""")
-    /**
-     * Extracts canonical names from the given item using regex.
-     *
-     * @param item The string containing the item.
-     * @return An array of canonical names found in the item.
-     */
-    fun getCanonicalName(input: String): List<String> {
-        val matches = CANONICAL_NAME_REGEX_PATTERN.findAll(input)
-        val canonicalNames = matches.map { it.value.substring(1, it.value.length - 1) }.toList()
-        return canonicalNames
-    }
-
     /**
      * Collects all the problems found in the given `project`, within the specified `editor` and `element`.
      *
