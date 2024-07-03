62f09f6399e03b5e14500b268341e99ad5ab80f1
Phodal Huang <h@phodal.com>
2024-07-03 12:16:45 +0800

    fix(run): handle line markers for leaf elements only
    Override `getLineMarkerInfo` to handle only leaf elements to prevent exceptions.

--- shirelang/src/main/kotlin/com/phodal/shirelang/run/ShireSyntaxLineMarkerProvider.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/run/ShireSyntaxLineMarkerProvider.kt
@@ -4,24 +4,23 @@ import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 import com.phodal.shirelang.ShireIcons
-import com.phodal.shirelang.psi.ShireActionBody
-import com.phodal.shirelang.psi.ShireCaseBody
-import com.phodal.shirelang.psi.ShireFrontMatterEntry
-import com.phodal.shirelang.psi.ShireQueryStatement
+import com.phodal.shirelang.psi.*
 
 class ShireSyntaxLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         if (element !is ShireFrontMatterEntry) return null
 
-        val key = element.frontMatterKey?.firstChild ?: return null
+        // only leaf elements can have line markers, or IDEA will throw an exception
+        val leafElement = getLeafElement(element) ?: return null
 
         val patternAction = element.patternAction
         if (patternAction != null) {
             val firstExpr = patternAction.actionBlock.actionBody.actionExprList.firstOrNull()
             when (firstExpr?.firstChild) {
                 is ShireCaseBody -> {
-                    return LineMarkerInfo(key, key.textRange, ShireIcons.Case, null, null, LEFT)
+                    return LineMarkerInfo(leafElement, leafElement.textRange, ShireIcons.Case, null, null, LEFT)
                     { "" }
                 }
             }
@@ -30,16 +29,31 @@ class ShireSyntaxLineMarkerProvider : LineMarkerProvider {
 
         when (element.functionStatement?.functionBody?.firstChild) {
             is ShireQueryStatement -> {
-                return LineMarkerInfo(key, key.textRange, ShireIcons.PsiExpr, null, null, LEFT)
+                return LineMarkerInfo(leafElement, leafElement.textRange, ShireIcons.PsiExpr, null, null, LEFT)
                 { "" }
             }
 
             is ShireActionBody -> {
-                return LineMarkerInfo(key, key.textRange, ShireIcons.Pipeline, null, null, LEFT)
+                return LineMarkerInfo(leafElement, leafElement.textRange, ShireIcons.Pipeline, null, null, LEFT)
                 { "" }
             }
         }
 
         return null
+    }
+
+    private fun getLeafElement(element: ShireFrontMatterEntry): PsiElement? {
+        val firstChild = element.frontMatterKey?.firstChild
+        when (firstChild?.elementType) {
+            ShireTypes.PATTERN,
+            ShireTypes.FRONT_MATTER_ID -> {
+                return firstChild?.firstChild ?: firstChild
+            }
+            ShireTypes.IDENTIFIER -> {
+                return firstChild
+            }
+        }
+
+        return firstChild
     }
 }
