cf334302e8222ee1c5c2bbf1f37e8e5bc1b05155
Phodal Huang <h@phodal.com>
2024-07-03 14:28:36 +0800

    style(completion): improve code readability with icon and priority
    Add icons and adjust priority for completion elements in WhenConditionCompletionProvider for better visual representation and ordering.

--- shirelang/src/main/kotlin/com/phodal/shirelang/completion/provider/WhenConditionCompletionProvider.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/completion/provider/WhenConditionCompletionProvider.kt
@@ -6,6 +6,7 @@ import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.util.ProcessingContext
+import com.phodal.shirelang.ShireIcons
 import com.phodal.shirelang.actions.validator.PsiVariables
 import com.phodal.shirelang.compiler.hobbit.ast.ExpressionBuiltInMethod
 
@@ -18,8 +19,10 @@ class WhenConditionCompletionProvider : CompletionProvider<CompletionParameters>
         PsiVariables.completionProvider().forEach {
             val withTypeText =
                 PrioritizedLookupElement.withPriority(
-                    LookupElementBuilder.create(it.key).withTypeText(it.value, true),
-                    99.0
+                    LookupElementBuilder.create(it.key)
+                        .withIcon(ShireIcons.DEFAULT)
+                        .withTypeText(it.value, true),
+                    199.0
                 )
 
             result.addElement(withTypeText)
@@ -34,7 +37,7 @@ class WhenConditionFunctionCompletionProvider : CompletionProvider<CompletionPar
         result: CompletionResultSet,
     ) {
         ExpressionBuiltInMethod.completionProvider().forEach {
-            val elementBuilder = LookupElementBuilder.create(it.name)
+            val elementBuilder = LookupElementBuilder.create(it.methodName)
                 .withTypeText(it.description, true)
                 .withInsertHandler { context, _ ->
                     context.document.insertString(context.tailOffset, it.postInsertString)
