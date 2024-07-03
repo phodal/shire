a830bc43a48eb3bf53d090ba39392ad320305ada
Phodal Huang <h@phodal.com>
2024-07-03 11:13:31 +0800

    refactor(documentation): improve code readability and structure
    Refactor the documentation provider code to improve readability and structure. Added logging and refactored the logic for generating documentation based on different element types.

--- shirelang/src/main/kotlin/com/phodal/shirelang/documentation/ShireDocumentationProvider.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/documentation/ShireDocumentationProvider.kt
@@ -1,6 +1,7 @@
 package com.phodal.shirelang.documentation
 
 import com.intellij.lang.documentation.AbstractDocumentationProvider
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -16,38 +17,48 @@ class ShireDocumentationProvider : AbstractDocumentationProvider() {
     override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
         val project = element?.project ?: return null
         val markdownDoc = when (element.elementType) {
-            ShireTypes.AGENT_ID -> {
-                val agentConfigs = CustomAgent.loadFromProject(project).filter {
-                    it.name == element.text
+            ShireTypes.IDENTIFIER -> {
+               when (element.parent.elementType) {
+                    ShireTypes.AGENT_ID -> {
+                        val agentConfigs = CustomAgent.loadFromProject(project).filter {
+                            it.name == element.text
+                        }
+
+                        if (agentConfigs.isEmpty()) return null
+                        agentConfigs.joinToString("\n") { it.description }
+                    }
+
+                    ShireTypes.COMMAND_ID -> {
+                        val command = BuiltinCommand.all().find { it.commandName == element.text } ?: return null
+                        val example = BuiltinCommand.example(command)
+                        val lang = ShireLanguage.INSTANCE.displayName
+                        "${command.description}\nExample:\n```$lang\n$example\n```\n "
+                    }
+
+                    ShireTypes.VARIABLE_ID -> {
+                        CompositeVariableProvider.all().find { it.name == element.text }?.description
+                    }
+
+                    else -> null
                 }
-
-                if (agentConfigs.isEmpty()) return null
-                agentConfigs.joinToString("\n") { it.description }
             }
 
-            ShireTypes.COMMAND_ID -> {
-                val command = BuiltinCommand.all().find { it.commandName == element.text } ?: return null
-                val example = BuiltinCommand.example(command)
-                val lang = ShireLanguage.INSTANCE.displayName
-                "${command.description}\nExample:\n```$lang\n$example\n```\n "
-            }
-
-            ShireTypes.VARIABLE_ID -> {
-                CompositeVariableProvider.all().find { it.name == element.text }?.description
+            ShireTypes.PATTERN_ACTION -> {
+                "Pattern action is a way to define a pattern for the agent to match. It's a JSONPath expression."
             }
 
             else -> {
-                element.text
+                null
             }
-        }
+        } ?: return null
 
-        return convertMarkdownToHtml(markdownDoc ?: "")
+        return convertMarkdownToHtml(markdownDoc)
     }
 
     override fun getCustomDocumentationElement(
         editor: Editor,
         file: PsiFile,
         contextElement: PsiElement?,
-        targetOffset: Int
+        targetOffset: Int,
     ): PsiElement? = contextElement ?: file.findElementAt(targetOffset)
 }
