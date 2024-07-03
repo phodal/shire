2c573380a50fa16687b667fa366cb6b88cc4c4dc
Phodal Huang <h@phodal.com>
2024-07-03 11:18:01 +0800

    refactor(completion): improve ContextVariable descriptions
    Update the descriptions of ContextVariable enum values for clarity and consistency.

--- shirelang/src/main/kotlin/com/phodal/shirelang/completion/dataprovider/ContextVariable.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/completion/dataprovider/ContextVariable.kt
@@ -1,14 +1,14 @@
 package com.phodal.shirelang.completion.dataprovider
 
 enum class ContextVariable(val variable: String, val description: String) {
-    SELECTION("selection", "The selected text"),
-    BEFORE_CURSOR("beforeCursor", "The text before the cursor"),
-    AFTER_CURSOR("afterCursor", "The text after the cursor"),
+    SELECTION("selection", "User selection code/element's in text"),
+    BEFORE_CURSOR("beforeCursor", "All the text before the cursor"),
+    AFTER_CURSOR("afterCursor", "All the text after the cursor"),
     FILE_NAME("fileName", "The name of the file"),
     FILE_PATH("filePath", "The path of the file"),
     METHOD_NAME("methodName", "The name of the method"),
-    LANGUAGE("language", "The language of the file"),
-    COMMENT_SYMBOL("commentSymbol", "The comment symbol of the language"),
+    LANGUAGE("language", "The language of the current file, will use IntelliJ's language ID"),
+    COMMENT_SYMBOL("commentSymbol", "The comment symbol of the language, for example, `//` in Java"),
     ALL("all", "All the text")
     ;
 }
