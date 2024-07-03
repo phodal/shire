a2d7d503ad412977363c80c0cced3d7fa283aedb
Phodal Huang <h@phodal.com>
2024-07-03 20:58:18 +0800

    refactor(actions): refactor VCS action group inheritance
    Refactor VCS action group to inherit from ActionGroup instead of ShireActionGroup for consistency and clarity.

--- shirelang/src/main/kotlin/com/phodal/shirelang/actions/vcs/ShireVcsActionGroup.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/actions/vcs/ShireVcsActionGroup.kt
@@ -1,6 +1,6 @@
 package com.phodal.shirelang.actions.vcs
 
-import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -8,25 +8,23 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.phodal.shirecore.action.ShireActionLocation
 import com.phodal.shirelang.ShireIcons
 import com.phodal.shirelang.actions.ShireRunFileAction
-import com.phodal.shirelang.actions.base.ShireActionGroup
 import com.phodal.shirelang.actions.dynamic.DynamicShireActionConfig
 import com.phodal.shirelang.actions.dynamic.DynamicShireActionService
 
-class ShireVcsActionGroup : ShireActionGroup() {
+class ShireVcsActionGroup : ActionGroup() {
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
     override fun update(e: AnActionEvent) {
-//        e.presentation.isPopupGroup = getActionsByType(e, ShireActionLocation.COMMIT_MENU).size > 1
+//        e.presentation.isPopupGroup = shireActionConfigs().size > 1
         e.presentation.isPopupGroup = true
     }
 
     override fun getChildren(e: AnActionEvent?): Array<AnAction> {
-        val configs: List<DynamicShireActionConfig> =
-            DynamicShireActionService.getInstance().getAction(ShireActionLocation.COMMIT_MENU)
-        return configs.map { actionConfig ->
-            ShireVcsAction(actionConfig)
-        }.toTypedArray()
+        return shireActionConfigs().map(::ShireVcsAction).toTypedArray()
     }
+
+    private fun shireActionConfigs() =
+        DynamicShireActionService.getInstance().getAction(ShireActionLocation.COMMIT_MENU)
 }
 
 class ShireVcsAction(val config: DynamicShireActionConfig) :
