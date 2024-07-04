01023de953d69115760d0b6c29a085f344a2f067
Phodal Huang <h@phodal.com>
2024-07-04 21:49:39 +0800

    feat(terminal): add ShireTerminalAction for custom assistants
    Add ShireTerminalAction class to enable custom assistants in ShireLang.

---
+++ shirelang/src/main/kotlin/com/phodal/shirelang/actions/terminal/ShireTerminalAction.kt
@@ -0,0 +1,27 @@
+package com.phodal.shirelang.actions.terminal
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.phodal.shirecore.config.ShireActionLocation
+import com.phodal.shirelang.actions.ShireRunFileAction
+import com.phodal.shirelang.actions.dynamic.DynamicShireActionService
+
+class ShireTerminalAction : DumbAwareAction() {
+    override fun getActionUpdateThread() = ActionUpdateThread.EDT
+
+    private fun shireActionConfigs() =
+        DynamicShireActionService.getInstance().getAction(ShireActionLocation.TERMINAL_MENU)
+
+    override fun update(e: AnActionEvent) {
+        e.presentation.isVisible = shireActionConfigs().size == 1
+        e.presentation.isEnabled = shireActionConfigs().size == 1
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+
+        val config = shireActionConfigs().first()
+        ShireRunFileAction.executeShireFile(project, config, null)
+    }
+}
