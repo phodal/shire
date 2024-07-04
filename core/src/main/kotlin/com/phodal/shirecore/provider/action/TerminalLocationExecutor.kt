03dce2625011eea61cd3b2b5e0860527a904229f
Phodal Huang <h@phodal.com>
2024-07-04 23:09:04 +0800

    feat(terminal): add input box popup for terminal action
    Add functionality to show an input box popup for the terminal action, allowing users to input commands before execution.

--- core/src/main/kotlin/com/phodal/shirecore/provider/action/TerminalLocationExecutor.kt
+++ core/src/main/kotlin/com/phodal/shirecore/provider/action/TerminalLocationExecutor.kt
@@ -1,10 +1,13 @@
 package com.phodal.shirecore.provider.action
 
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import com.phodal.shirecore.provider.action.terminal.TerminalHandler
+import java.awt.Component
 
 interface TerminalLocationExecutor {
+    fun getComponent(e: AnActionEvent): Component?
     fun buildBundler(): TerminalHandler?
     fun getUserInput(): String?
 
---
+++ toolsets/terminal/src/main/kotlin/com/phodal/shire/terminal/ShireTerminalExecutor.kt
@@ -0,0 +1,22 @@
+package com.phodal.shire.terminal
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.phodal.shirecore.provider.action.TerminalLocationExecutor
+import com.phodal.shirecore.provider.action.terminal.TerminalHandler
+import java.awt.Component
+
+class ShireTerminalExecutor : TerminalLocationExecutor {
+    override fun getComponent(e: AnActionEvent): Component? {
+        return e.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT)
+    }
+
+    override fun buildBundler(): TerminalHandler? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserInput(): String? {
+        TODO("Not yet implemented")
+    }
+
+}
