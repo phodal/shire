026c9d0b97207df37860ee1f223afe1829ab32cb
Phodal Huang <h@phodal.com>
2024-07-05 09:12:15 +0800

    refactor(terminal): remove unused imports and methods
    Clean up unnecessary imports and methods in ShireTerminalExecutor.kt.

--- toolsets/terminal/src/main/kotlin/com/phodal/shire/terminal/ShireTerminalExecutor.kt
+++ toolsets/terminal/src/main/kotlin/com/phodal/shire/terminal/ShireTerminalExecutor.kt
@@ -1,20 +1,14 @@
 package com.phodal.shire.terminal
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
-import com.intellij.openapi.application.runInEdt
-import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.terminal.ui.TerminalWidget
-import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.content.Content
 import com.phodal.shirecore.provider.action.TerminalLocationExecutor
 import com.phodal.shirecore.provider.action.terminal.TerminalHandler
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
-import org.jetbrains.plugins.terminal.exp.TerminalPromptController
 import java.awt.Component
 
 class ShireTerminalExecutor : TerminalLocationExecutor {
@@ -24,36 +18,7 @@ class ShireTerminalExecutor : TerminalLocationExecutor {
 
     override fun bundler(project: Project, userInput: String): TerminalHandler? {
         val content = getContent(project) ?: return null
-        val findWidgetByContent = TerminalToolWindowManager.findWidgetByContent(content) ?: return null
-        val controller: TerminalPromptController? = lookupTerminalPromptControllerByView(findWidgetByContent)
-
-        if (controller == null) {
-            return trySendMsgInOld(project, userInput, content)
-        }
-
-        val sb = StringBuilder()
-        return TerminalHandler(
-            userInput,
-            project,
-            onChunk = { string ->
-                sb.append(string)
-            },
-            onFinish = {
-                runInEdt {
-                    CopyPasteManager.copyTextToClipboard(sb.toString())
-                    controller.performPaste()
-                }
-            })
-    }
-
-    private fun lookupTerminalPromptControllerByView(findWidgetByContent: TerminalWidget): TerminalPromptController? {
-        val terminalView = (findWidgetByContent.component as? Wrapper)?.targetComponent ?: return null
-        if (terminalView is DataProvider) {
-            val controller = terminalView.getData(TerminalPromptController.KEY.name)
-            return (controller as? TerminalPromptController)
-        }
-
-        return null
+        return trySendMsgInOld(project, userInput, content)
     }
 
     private fun trySendMsgInOld(project: Project, userInput: String, content: Content): TerminalHandler? {
