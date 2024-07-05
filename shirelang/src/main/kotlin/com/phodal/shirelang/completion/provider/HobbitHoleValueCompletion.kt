2df13189751e4fa1b28751ab0a9dc5a14d83ad4d
Phodal Huang <h@phodal.com>
2024-07-05 19:47:51 +0800

    refactor(completion): improve insert handler logic
    Refactored the insert handler logic in HobbitHoleValueCompletion.kt to handle insertion of values inside `{ }` for better user experience.

--- shirelang/src/main/kotlin/com/phodal/shirelang/completion/provider/HobbitHoleValueCompletion.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/completion/provider/HobbitHoleValueCompletion.kt
@@ -3,6 +3,8 @@ package com.phodal.shirelang.completion.provider
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
@@ -71,14 +73,22 @@ class HobbitHoleValueCompletion : CompletionProvider<CompletionParameters>() {
                     )
                 }
             }
+
             HobbitHole.ON_STREAMING_END -> {
                 PostProcessor.allNames().forEach {
                     result.addElement(
                         LookupElementBuilder
                             .create(it)
                             .withIcon(ShireIcons.DEFAULT)
-                            .withInsertHandler { context, _ ->
-                                context.document.insertString(context.startOffset, " ")
+                            .withInsertHandler { context: InsertionContext, item: LookupElement ->
+                                val offset = context.editor.caretModel.offset
+                                val startOffset = offset - item.lookupString.length
+                                context.document.deleteString(startOffset, offset)
+                                // insert value inside `{ }`, for example, if user select $demo, the insert will be `{ $demo }`
+                                val value = " { $it  }"
+                                context.document.insertString(context.startOffset, value)
+
+                                context.editor.caretModel.moveToOffset(startOffset + value.length - 2)
                             }
                     )
                 }
