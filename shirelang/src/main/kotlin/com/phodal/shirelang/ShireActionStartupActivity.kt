7ed2aabe8cae183e68c6aff4092af591ede03fae
Phodal Huang <h@phodal.com>
2024-07-05 09:35:03 +0800

    refactor(actions): refactor action binding logic
    Refactor action binding logic to improve readability and maintainability. Added a method to attach terminal action manually.

--- shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/ShireActionStartupActivity.kt
@@ -1,13 +1,10 @@
 package com.phodal.shirelang
 
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.keymap.KeymapManager
-import com.intellij.openapi.keymap.impl.KeymapImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.psi.PsiManager
@@ -23,6 +20,10 @@ class ShireActionStartupActivity : ProjectActivity {
     private val logger = logger<ShireActionStartupActivity>()
 
     override suspend fun execute(project: Project) {
+        bindingShireActions(project)
+    }
+
+    private suspend fun bindingShireActions(project: Project) {
         smartReadAction(project) {
             obtainShireFiles(project).forEach {
                 val shireConfig = try {
@@ -35,7 +36,21 @@ class ShireActionStartupActivity : ProjectActivity {
                 val shireActionConfig = DynamicShireActionConfig(shireConfig.name, shireConfig, it)
                 DynamicShireActionService.getInstance().putAction(shireConfig.name, shireActionConfig)
             }
+
+            attachTerminalAction()
         }
+    }
+
+    /**
+     * We make terminal plugin optional, so can't add to `TerminalToolwindowActionGroup` the plugin.xml.
+     * So we add it manually here, if terminal plugin is not enabled, this action will not be shown.
+     */
+    private fun attachTerminalAction() {
+        val actionManager = ActionManager.getInstance()
+        val toolsMenu = actionManager.getAction("TerminalToolwindowActionGroup") as? DefaultActionGroup
+
+        val action = actionManager.getAction("ShireTerminalAction")
+        toolsMenu?.add(action)
     }
 
     companion object {
