8b977163a84342b91205f2f2a244f814fe5022fc
Phodal Huang <h@phodal.com>
2024-07-14 21:11:34 +0800

    style(actions): 更新ShireContextMenuAction的显示文本
    为了解决空菜单项文本的问题，更新了ShireContextMenuAction的显示文本设置方式。现在使用配置的名称，如果为空则显示"Unknown"。移除了不必要的let函数调用。

--- shirelang/src/main/kotlin/com/phodal/shirelang/actions/context/ShireContextMenuAction.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/actions/context/ShireContextMenuAction.kt
@@ -8,22 +8,22 @@ import com.phodal.shirelang.ShireIcons
 import com.phodal.shirelang.actions.ShireRunFileAction
 import com.phodal.shirelang.actions.base.DynamicShireActionConfig
 import com.phodal.shirelang.actions.validator.WhenConditionValidator
-import kotlin.let
 
 class ShireContextMenuAction(private val config: DynamicShireActionConfig) :
     DumbAwareAction(config.name, config.hole?.description, ShireIcons.DEFAULT) {
+
+    init {
+        templatePresentation.text = config.name.ifBlank { "Unknown" }
+    }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.BGT
     }
 
 
-
     override fun update(e: AnActionEvent) {
         //2024-07-13 10:32:57,277 [51307999] SEVERE - #c.i.o.a.i.Utils - Empty menu item text for ShireContextMenuAction@EditorPopup (com.phodal.shirelang.actions.context.ShireContextMenuAction). The default action text must be specified in plugin.xml or its class constructor [Plugin: com.phodal.shire]
         // com.intellij.diagnostic.PluginException: Empty menu item text for ShireContextMenuAction@EditorPopup (com.phodal.shirelang.actions.context.ShireContextMenuAction). The default action text must be specified in plugin.xml or its class constructor [Plugin: com.phodal.shire]
-        e.presentation.text = config.hole?.name ?: "Unknown"
-
         val conditions = config.hole?.when_ ?: return
         val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
 
