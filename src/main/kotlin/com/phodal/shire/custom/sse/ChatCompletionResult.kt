ec7167bf96d03e486d1098bcbe8d0f0378ea6f1c
Phodal Huang <h@phodal.com>
2024-07-20 09:52:04 +0800

    refactor(sse): refactor ChatCompletionResult structure
    Refactor the structure of ChatCompletionResult to use data classes for better readability and maintainability.

--- src/main/kotlin/com/phodal/shire/custom/sse/ChatCompletionResult.kt
+++ src/main/kotlin/com/phodal/shire/custom/sse/ChatCompletionResult.kt
@@ -1,36 +1,46 @@
 package com.phodal.shire.custom.sse
 
-import com.theokanning.openai.Usage
-import com.theokanning.openai.completion.chat.ChatCompletionChoice
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
 
-class ChatCompletionResult {
-    /**
-     * Unique id assigned to this chat completion.
-     */
-    var id: String? = null
+data class ChatFunctionCall(
+    val name: String? = null,
+    val arguments: JsonNode? = null,
+)
 
-    /**
-     * The type of object returned, should be "chat.completion"
-     */
-    var `object`: String? = null
+data class ChatMessage(
+    val role: String,
+    @JsonInclude
+    val content: String,
+    val name: String? = null,
+    @JsonProperty("function_call")
+    val functionCall: ChatFunctionCall? = null,
+)
 
-    /**
-     * The creation time in epoch seconds.
-     */
-    var created: Long = 0
+data class ChatCompletionChoice(
+    val index: Int? = null,
+    @JsonAlias("delta")
+    val message: ChatMessage? = null,
+    @JsonProperty("finish_reason")
+    val finishReason: String? = null,
+)
 
-    /**
-     * The GPT model used.
-     */
-    var model: String? = null
+data class Usage(
+    @JsonProperty("prompt_tokens")
+    val promptTokens: Long = 0,
+    @JsonProperty("completion_tokens")
+    val completionTokens: Long = 0,
+    @JsonProperty("total_tokens")
+    val totalTokens: Long = 0,
+)
 
-    /**
-     * A list of all generated completions.
-     */
-    var choices: List<ChatCompletionChoice> = emptyList()
-
-    /**
-     * The API usage for this request.
-     */
-    var usage: Usage? = null
-}
+data class ChatCompletionResult(
+    val id: String? = null,
+    val `object`: String? = null,
+    val created: Long = 0,
+    val model: String? = null,
+    val choices: List<ChatCompletionChoice>,
+    val usage: Usage? = null,
+)
