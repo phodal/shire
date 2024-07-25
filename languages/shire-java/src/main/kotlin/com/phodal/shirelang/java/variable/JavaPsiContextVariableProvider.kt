d203b5c1c43b0745974be079fbff6b88ead41d5a
Phodal Huang <h@phodal.com>
2024-07-25 09:23:34 +0800

    refactor(shirelang): streamline PsiContextVariableProvider logic
    Combine PsiContextVariable cases into a single, more concise switch-case block.

--- languages/shire-java/src/main/kotlin/com/phodal/shirelang/java/variable/JavaPsiContextVariableProvider.kt
+++ languages/shire-java/src/main/kotlin/com/phodal/shirelang/java/variable/JavaPsiContextVariableProvider.kt
@@ -9,6 +9,7 @@ import com.phodal.shirecore.provider.context.ToolchainPrepareContext
 import com.phodal.shirecore.provider.variable.impl.CodeSmellBuilder
 import com.phodal.shirecore.provider.variable.model.PsiContextVariable
 import com.phodal.shirecore.provider.variable.PsiContextVariableProvider
+import com.phodal.shirecore.provider.variable.model.PsiContextVariable.*
 import com.phodal.shirecore.search.SimilarChunksSearch
 import com.phodal.shirelang.java.codemodel.JavaClassStructureProvider
 import com.phodal.shirelang.java.util.JavaTestHelper
@@ -24,33 +25,19 @@ class JavaPsiContextVariableProvider : PsiContextVariableProvider {
         val sourceFile: PsiJavaFile = psiElement.containingFile as PsiJavaFile
 
         return when (variable) {
-            PsiContextVariable.IMPORTS -> sourceFile.importList?.text ?: ""
+            IMPORTS -> sourceFile.importList?.text ?: ""
+            CURRENT_CLASS_NAME -> clazz?.name ?: ""
+            CURRENT_CLASS_CODE -> sourceFile.text
+            CURRENT_METHOD_NAME -> (psiElement as? PsiMethod)?.name ?: ""
+            CURRENT_METHOD_CODE -> (psiElement as? PsiMethod)?.body?.text ?: ""
+            RELATED_CLASSES -> JavaRelatedClassesProvider().lookup(psiElement.parent).joinToString("\n") { it.text }
+            SIMILAR_TEST_CASE -> JavaTestHelper.searchSimilarTestCases(psiElement).joinToString("\n") { it.text }
+            IS_NEED_CREATE_FILE -> TestFinderHelper.findClassesForTest(psiElement).isEmpty()
+            TARGET_TEST_FILE_NAME -> sourceFile.name.replace(".java", "") + "Test.java"
+            UNDER_TEST_METHOD_CODE -> JavaTestHelper.extractMethodCalls(project, psiElement)
+            CODE_SMELL -> CodeSmellBuilder.collectElementProblemAsSting(psiElement, project, editor)
 
-            PsiContextVariable.CURRENT_CLASS_NAME -> clazz?.name ?: ""
-
-            PsiContextVariable.CURRENT_CLASS_CODE -> sourceFile.text
-
-            PsiContextVariable.CURRENT_METHOD_NAME -> (psiElement as? PsiMethod)?.name ?: ""
-
-            PsiContextVariable.CURRENT_METHOD_CODE -> (psiElement as? PsiMethod)?.body?.text ?: ""
-
-            PsiContextVariable.RELATED_CLASSES -> {
-                JavaRelatedClassesProvider().lookup(psiElement.parent).joinToString("\n") { it.text }
-            }
-
-            PsiContextVariable.SIMILAR_TEST_CASE -> {
-                JavaTestHelper.searchSimilarTestCases(psiElement).joinToString("\n") { it.text }
-            }
-
-            PsiContextVariable.IS_NEED_CREATE_FILE -> TestFinderHelper.findClassesForTest(psiElement).isEmpty()
-
-            PsiContextVariable.TARGET_TEST_FILE_NAME -> sourceFile.name.replace(".java", "") + "Test.java"
-
-            PsiContextVariable.UNDER_TEST_METHOD_CODE -> {
-                JavaTestHelper.extractMethodCalls(project, psiElement)
-            }
-
-            PsiContextVariable.FRAMEWORK_CONTEXT -> {
+            FRAMEWORK_CONTEXT -> {
                 runBlocking {
                     val prepareContext = ToolchainPrepareContext(sourceFile, psiElement)
                     val contextItems =
@@ -59,23 +46,20 @@ class JavaPsiContextVariableProvider : PsiContextVariableProvider {
                     contextItems.joinToString("\n") { it.text }
                 }
             }
-
-            PsiContextVariable.CODE_SMELL -> CodeSmellBuilder.collectElementProblemAsSting(psiElement, project, editor)
-            PsiContextVariable.METHOD_CALLER -> {
+            METHOD_CALLER -> {
                 if (psiElement !is PsiMethod) return ""
                 return JavaTestHelper.findCallers(psiElement).joinToString("\n") { it.text }
             }
-            PsiContextVariable.CALLED_METHOD -> {
+
+            CALLED_METHOD -> {
                 if (psiElement !is PsiMethod) return ""
                 return JavaTestHelper.findCallees(psiElement).joinToString("\n") { it.text }
             }
 
-            PsiContextVariable.SIMILAR_CODE -> return SimilarChunksSearch.createQuery(psiElement) ?: ""
-            PsiContextVariable.STRUCTURE -> {
-                clazz?.let {
-                    JavaClassStructureProvider().build(it, true)?.toString() ?: ""
-                } ?: ""
-            }
+            SIMILAR_CODE -> return SimilarChunksSearch.createQuery(psiElement) ?: ""
+            STRUCTURE -> clazz?.let {
+                JavaClassStructureProvider().build(it, true)?.toString() ?: ""
+            } ?: ""
         }
     }
 }
