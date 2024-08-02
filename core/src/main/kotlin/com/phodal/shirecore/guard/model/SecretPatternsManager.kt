75c3bbadfd05eb836025955fe2617b541c23478b
Phodal Huang <h@phodal.com>
2024-08-02 13:59:08 +0800

    refactor(secrets): convert field to property with service annotation #47
    This change modifies the `SecretPatternsManager` class by converting the `patterns` field into a property with the `@Service` annotation. The initialization of the patterns is now done within an `init` block, ensuring proper lazy initialization. Additionally, the getter for the `patterns` has been moved up in the file for clarity.

--- core/src/main/kotlin/com/phodal/shirecore/guard/model/SecretPatternsManager.kt
+++ core/src/main/kotlin/com/phodal/shirecore/guard/model/SecretPatternsManager.kt
@@ -1,11 +1,21 @@
 package com.phodal.shirecore.guard.model
 
 import com.charleskorn.kaml.Yaml
+import com.intellij.openapi.components.Service
 import java.net.URL
 
+@Service(Service.Level.APP)
 class SecretPatternsManager {
     private val defaultPiiSecrets = "/secrets/default.shireSecretPattern.yml"
-    private var patterns: List<SecretPatternDetail> = initPatterns()
+    private var patterns: List<SecretPatternDetail>
+
+    init {
+        patterns = initPatterns()
+    }
+
+    fun getPatterns(): List<SecretPatternDetail> {
+        return patterns
+    }
 
     private fun initPatterns(): List<SecretPatternDetail> {
         val file: URL = javaClass.getResource(defaultPiiSecrets)!!
@@ -15,10 +25,6 @@ class SecretPatternsManager {
         return patterns.patterns.map {
             it.pattern
         }
-    }
-
-    fun getPatterns(): List<SecretPatternDetail> {
-        return patterns
     }
 
     fun addPattern(pattern: SecretPatternDetail) {
