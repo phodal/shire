c8999e59d6ea9fac58a59e2864680d89ec163253
Phodal Huang <h@phodal.com>
2024-08-02 14:41:26 +0800

    feat(schema-provider): add CustomAgentSchemaFileProvider to factory list #47

--- core/src/main/kotlin/com/phodal/shirecore/agent/schema/ShireCustomAgentSchemaProviderFactory.kt
+++ core/src/main/kotlin/com/phodal/shirecore/agent/schema/ShireCustomAgentSchemaProviderFactory.kt
@@ -6,6 +6,9 @@ import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 
 class ShireCustomAgentSchemaProviderFactory : JsonSchemaProviderFactory  {
     override fun getProviders(project: Project): MutableList<JsonSchemaFileProvider> {
-        return mutableListOf(SecretPatternSchemaFileProvider(project))
+        return mutableListOf(
+            SecretPatternSchemaFileProvider(project),
+            CustomAgentSchemaFileProvider(project)
+        )
     }
 }
