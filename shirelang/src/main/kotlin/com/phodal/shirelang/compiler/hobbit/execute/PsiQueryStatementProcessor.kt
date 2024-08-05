0d87ed12bc89a699a996f03127fb80b3a5e11613
Phodal Huang <h@phodal.com>
2024-08-05 21:11:11 +0800

    refactor(compiler): improve error handling in PsiQueryStatementProcessor
    Replace direct error logging with a dedicated method for showing error logs, improving code structure and readability.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/PsiQueryStatementProcessor.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/PsiQueryStatementProcessor.kt
@@ -7,6 +7,7 @@ import com.intellij.psi.PsiElement
 import com.phodal.shirecore.provider.variable.PsiQLInterpreter
 import com.phodal.shirelang.compiler.hobbit.HobbitHole
 import com.phodal.shirelang.compiler.hobbit.ast.MethodCall
+import java.lang.reflect.Method
 import java.util.*
 
 class PsiQueryStatementProcessor(override val myProject: Project, hole: HobbitHole) :
@@ -15,7 +16,7 @@ class PsiQueryStatementProcessor(override val myProject: Project, hole: HobbitHo
     override fun <T : Any> invokeMethodOrField(methodCall: MethodCall, element: T): Any? {
         val methodName = methodCall.methodName.display()
         val methodArgs = methodCall.arguments
-        if(element is PsiElement) {
+        if (element is PsiElement) {
             PsiQLInterpreter.provide(element.language)?.let { psiQLInterpreter ->
                 val hasPqlInterpreter = psiQLInterpreter.supportsMethod(element.language, methodName).filter {
                     it == methodName
@@ -71,19 +72,24 @@ class PsiQueryStatementProcessor(override val myProject: Project, hole: HobbitHo
             }
         }
 
-        val supportMethodNames: List<String> = allMethods.map {
-            it.name
-        }
-        val supportFieldNames: List<String> = element.javaClass.fields.map {
-            it.name
-        }
+        // if not found, show error log
+        return showErrorLog(allMethods, element, methodName)
+    }
+
+    private fun <T : Any> showErrorLog(
+        allMethods: Array<out Method>,
+        element: T,
+        methodName: String,
+    ): Nothing? {
+        val supportMethodNames: List<String> = allMethods.map { it.name }
+        val supportFieldNames: List<String> = element.javaClass.fields.map { it.name }
 
         logger<PsiQueryStatementProcessor>().error(
             """
-            method or field not found: $methodName
-            supported methods: $supportMethodNames
-            supported fields: $supportFieldNames
-            """.trimIndent()
+                method or field not found: $methodName
+                supported methods: $supportMethodNames
+                supported fields: $supportFieldNames
+                """.trimIndent()
         )
         return null
     }
