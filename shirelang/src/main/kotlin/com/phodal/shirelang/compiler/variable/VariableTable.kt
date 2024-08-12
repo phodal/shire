71d25ed30eec227b84e7f889134df27aea9c5203
Phodal Huang <h@phodal.com>
2024-08-12 12:45:28 +0800

    refactor(compiler): move inner classes outside of method scope

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/variable/VariableTable.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/variable/VariableTable.kt
@@ -45,21 +45,21 @@ class VariableTable {
     }
 
     fun getAllVariables(): Map<String, VariableInfo> = table.toMap()
-}
 
-data class VariableInfo(
-    val type: VariableType,
-    val scope: VariableScope,
-    val lineDeclared: Int
-)
+    data class VariableInfo(
+        val type: VariableType,
+        val scope: VariableScope,
+        val lineDeclared: Int
+    )
 
-enum class VariableType {
-    String,
-    Boolean,
-    Number,
-}
+    enum class VariableType {
+        String,
+        Boolean,
+        Number,
+    }
 
-enum class VariableScope {
-    BuiltIn,
-    UserDefined
+    enum class VariableScope {
+        BuiltIn,
+        UserDefined
+    }
 }
