a5a505b422595738131d65e6bd04f9872091e702
Phodal Huang <h@phodal.com>
2024-08-13 20:37:20 +0800

    feat(sonarqube): implement SonarlintProvider for analyzing current file #54

---
+++ toolsets/sonarqube/src/main/kotlin/com/phodal/shire/sonarqube/SonarlintProvider.kt
@@ -0,0 +1,58 @@
+package com.phodal.shire.sonarqube
+
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ex.ActionUtil.invokeAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.treeStructure.Tree
+import org.sonarlint.intellij.cayc.CleanAsYouCodeService
+import org.sonarlint.intellij.common.util.SonarLintUtils
+import org.sonarlint.intellij.finding.issue.LiveIssue
+import org.sonarlint.intellij.ui.CurrentFilePanel
+import org.sonarlint.intellij.ui.tree.IssueTree
+import org.sonarlint.intellij.ui.tree.IssueTreeModelBuilder
+import org.sonarlint.intellij.util.SonarLintActions
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.util.Map
+import java.util.function.Predicate
+
+object SonarlintProvider {
+    private val analyzeCurrentFileAction: AnAction = SonarLintActions.getInstance().analyzeCurrentFileAction()
+
+    fun getSonarlintVersion(project: Project, file: VirtualFile): String {
+        var treeBuilder: IssueTreeModelBuilder = IssueTreeModelBuilder(project)
+        val model = treeBuilder.createModel(false)
+        val tree: Tree = IssueTree(project, model)
+
+        val statusText = StringBuilder()
+        val dataContext: DataContext = DataManager.getInstance().dataContextFromFocusAsync.blockingGet(1000)
+            ?: return statusText.toString()
+        val templateText = analyzeCurrentFileAction.templateText
+        if (templateText != null) {
+            ActionListener { ignore: ActionEvent? ->
+                invokeAction(
+                    analyzeCurrentFileAction,
+                    dataContext, CurrentFilePanel.SONARLINT_TOOLWINDOW_ID, null, null
+                )
+            }
+        }
+
+        val currentIssues: Collection<LiveIssue> = listOf()
+
+        if (SonarLintUtils.getService(CleanAsYouCodeService::class.java)
+                .shouldFocusOnNewCode(project)
+        ) {
+            val oldIssues: List<LiveIssue> =
+                currentIssues.stream().filter(Predicate.not<LiveIssue> { obj: LiveIssue -> obj.isOnNewCode() })
+                    .toList()
+            val newIssues: List<LiveIssue> =
+                currentIssues.stream().filter { obj: LiveIssue -> obj.isOnNewCode() }.toList()
+
+        }
+
+        return statusText.toString()
+    }
+}
