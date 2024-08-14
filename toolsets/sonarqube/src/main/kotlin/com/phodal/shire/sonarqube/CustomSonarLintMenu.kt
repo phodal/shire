cb2a4016ee4d9a64d533fc3307d60da0de65284d
Phodal Huang <h@phodal.com>
2024-08-14 16:23:03 +0800

    feat(sonarqube): add CustomSonarLintMenu and extension provider #54
    Add a new Kotlin class for a custom SonarLint menu and include the corresponding extension variable provider. The menu action is prepared but not yet implemented.
    
    - New file `CustomSonarLintMenu.kt` created for menu functionalities.
    - Extension provider implementation added to `sonarqube.xml` for toolchain variable support.
    - Menu action definition is currently commented out, pending further implementation.

---
+++ toolsets/sonarqube/src/main/kotlin/com/phodal/shire/sonarqube/CustomSonarLintMenu.kt
@@ -0,0 +1,10 @@
+package com.phodal.shire.sonarqube
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class CustomSonarLintMenu : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
