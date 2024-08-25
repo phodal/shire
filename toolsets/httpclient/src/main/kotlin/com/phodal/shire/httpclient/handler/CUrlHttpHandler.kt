f8c2936aa6f7925491d8fb605af759bf71b2a41f
Phodal Huang <h@phodal.com>
2024-08-25 20:00:25 +0800

    refactor(httpclient): enhance getSearchScope function in CUrlHttpHandler #11
    This commit updates the `CUrlHttpHandler` by adding a new method `getSearchScope` to improve the search functionality. The method now considers the context file, allowing for a more specific search scope, including special handling for "What's New" HTTP client files and scratch files. This change promotes better scoping for the variables retrieval process.

--- toolsets/httpclient/src/main/kotlin/com/phodal/shire/httpclient/handler/CUrlHttpHandler.kt
+++ toolsets/httpclient/src/main/kotlin/com/phodal/shire/httpclient/handler/CUrlHttpHandler.kt
@@ -1,7 +1,18 @@
 package com.phodal.shire.httpclient.handler
 
+import com.intellij.httpClient.http.request.HttpRequestCollectionProvider
+import com.intellij.httpClient.http.request.environment.HttpRequestIndex
+import com.intellij.httpClient.http.request.notification.HttpClientWhatsNewContentService
+import com.intellij.ide.scratch.ScratchUtil
+import com.intellij.ide.scratch.ScratchesSearchScope
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.GlobalSearchScopesCore
 import com.intellij.psi.search.ProjectScope
+import com.intellij.psi.util.PsiUtilCore
 import com.intellij.util.indexing.FileBasedIndex
 import com.phodal.shire.httpclient.converter.CUrlConverter
 import com.phodal.shirecore.index.SHIRE_ENV_ID
@@ -17,12 +28,38 @@ class CUrlHttpHandler : HttpHandler {
         val request = CUrlConverter.convert(project, content)
         val response = client.newCall(request).execute()
 
-        val variables = FileBasedIndex.getInstance().getValues<String, Set<String>>(
+        val variables: MutableList<Set<String>> = FileBasedIndex.getInstance().getValues(
             SHIRE_ENV_ID,
             "development",
-            ProjectScope.getContentScope(project)
+            getSearchScope(project)
         )
 
         return response.body?.string()
+    }
+
+    private fun getSearchScope(project: Project, contextFile: PsiFile? = null): GlobalSearchScope {
+        val projectScope = ProjectScope.getContentScope(project)
+        if (contextFile == null) return projectScope
+
+        val context = PsiUtilCore.getVirtualFile(contextFile)
+        val whatsNewFile = HttpClientWhatsNewContentService.getInstance().getWhatsNewFileIfCreated()
+
+        if (contextFile.virtualFile == whatsNewFile) {
+            HttpRequestCollectionProvider.getCollectionFolder()?.let { folder ->
+                return GlobalSearchScopesCore.directoryScope(project, folder, true)
+            }
+        }
+
+        if (context != null && !ScratchUtil.isScratch(context) && !projectScope.contains(context)) {
+            contextFile.parent?.let { parent ->
+                return GlobalSearchScopesCore.directoryScope(parent, true)
+            }
+        }
+
+        if (ScratchUtil.isScratch(context)) {
+            return projectScope.uniteWith(ScratchesSearchScope.getScratchesScope(project))
+        }
+
+        return projectScope
     }
 }
