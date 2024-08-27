a67da886b0c780cc9007018eef32928d3a6268fd
Phodal Huang <h@phodal.com>
2024-08-27 13:28:02 +0800

    refactor(database): enhance variable resolution and column retrieval #9
    Improve the variable resolution mechanism in `DatabaseVariableProvider` by utilizing the new methods in `DatabaseVariableBuilder`. This includes more precise database, table, and column information retrieval. Additionally, refactor the column retrieval process to provide a more detailed output.

--- toolsets/database/src/main/kotlin/com/phodal/shire/database/DatabaseVariableBuilder.kt
+++ toolsets/database/src/main/kotlin/com/phodal/shire/database/DatabaseVariableBuilder.kt
@@ -3,27 +3,43 @@ package com.phodal.shire.database
 import com.intellij.database.model.DasTable
 import com.intellij.database.model.ObjectKind
 import com.intellij.database.model.RawDataSource
+import com.intellij.database.psi.DbDataSource
 import com.intellij.database.psi.DbPsiFacade
 import com.intellij.database.util.DasUtil
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFile
-import com.intellij.sql.dialects.sqlite.SqliteDialect
 
 object DatabaseVariableBuilder {
-    fun getTables(project: Project, file: PsiFile): List<DasTable> {
+    fun getDatabases(project: Project): List<DbDataSource> {
+        val dbPsiFacade = DbPsiFacade.getInstance(project)
+        return dbPsiFacade.dataSources.toList()
+    }
+
+    fun getTables(project: Project): List<DasTable> {
         val rawDataSource = retrieveFirstRawDataSource(project) ?: return emptyList()
         val schemaName = rawDataSource.name.substringBeforeLast('@')
         val dasTables = rawDataSource.let {
-            val tables = DasUtil.getTables(it)
-            tables.filter { table -> table.kind == ObjectKind.TABLE &&
-                    (table.dasParent?.name == schemaName || (file.language == SqliteDialect.INSTANCE && table.dasParent?.name == "main"))
+            DasUtil.getTables(it).filter { table ->
+                table.kind == ObjectKind.TABLE && (table.dasParent?.name == schemaName)
             }.toList()
         }.toList()
 
         return dasTables
     }
 
-    fun getTableColumns(dasTables: List<DasTable>, tables: List<String>): List<String> {
+    fun getTableColumns(project: Project, tables: List<String> = emptyList()): List<String> {
+        val dasTables = getTables(project)
+
+        if (dasTables.isEmpty()) {
+            return dasTables.map {
+                val dasColumns = DasUtil.getColumns(it)
+                val columns = dasColumns.map {
+                    "${it.name}: ${it.dasType.toDataType()}"
+                }.joinToString(", ")
+
+                "TableName: ${it.name}, Columns: $columns"
+            }
+        }
+
         return dasTables.mapNotNull { tableName ->
             if (tables.contains(tableName.name)) {
                 val dasColumns = DasUtil.getColumns(tableName)
--- toolsets/database/src/main/kotlin/com/phodal/shire/database/provider/DatabaseVariableProvider.kt
+++ toolsets/database/src/main/kotlin/com/phodal/shire/database/provider/DatabaseVariableProvider.kt
@@ -3,6 +3,7 @@ package com.phodal.shire.database.provider
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.phodal.shire.database.DatabaseVariableBuilder
 import com.phodal.shirecore.provider.variable.ToolchainVariableProvider
 import com.phodal.shirecore.provider.variable.model.ToolchainVariable
 import com.phodal.shirecore.provider.variable.model.toolchain.DatabaseToolchainVariable
@@ -14,9 +15,9 @@ class DatabaseVariableProvider : ToolchainVariableProvider {
 
     override fun resolve(variable: ToolchainVariable, project: Project, editor: Editor, psiElement: PsiElement?): Any {
        return when (variable) {
-            DatabaseToolchainVariable.Databases -> "databases"
-            DatabaseToolchainVariable.Tables -> "tables"
-            DatabaseToolchainVariable.Columns -> "columns"
+            DatabaseToolchainVariable.Databases -> DatabaseVariableBuilder.getDatabases(project)
+            DatabaseToolchainVariable.Tables -> DatabaseVariableBuilder.getTables(project)
+            DatabaseToolchainVariable.Columns -> DatabaseVariableBuilder.getTableColumns(project)
             else -> ""
         }
     }
