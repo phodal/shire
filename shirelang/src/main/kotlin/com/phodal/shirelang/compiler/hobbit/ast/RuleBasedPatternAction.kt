a002f0402ca68d6b28047cdc01a53444c387337e
Phodal Huang <h@phodal.com>
2024-09-02 09:07:35 +0800

    refactor(shirelang): implement DirectAction base class for RuleBasedPatternAction

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/ast/RuleBasedPatternAction.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/ast/RuleBasedPatternAction.kt
@@ -2,4 +2,7 @@ package com.phodal.shirelang.compiler.hobbit.ast
 
 import com.phodal.shirelang.compiler.patternaction.PatternActionFunc
 
-class RuleBasedPatternAction(val pattern: String, val processors: List<PatternActionFunc>)
+class RuleBasedPatternAction(val pattern: String, override val processors: List<PatternActionFunc>) :
+    DirectAction(processors)
+
+open class DirectAction(open val processors: List<PatternActionFunc>)
