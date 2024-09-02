a45c239ed93f2294155a2c6c6359c625c92be5b1
Phodal Huang <h@phodal.com>
2024-09-02 17:23:20 +0800

    refactor(middleware): enhance OpenFileProcessor to handle string paths

--- core/src/main/kotlin/com/phodal/shirecore/middleware/builtin/OpenFileProcessor.kt
+++ core/src/main/kotlin/com/phodal/shirecore/middleware/builtin/OpenFileProcessor.kt
@@ -4,7 +4,9 @@ import com.intellij.execution.ui.ConsoleView
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.phodal.shirecore.middleware.BuiltinPostHandler
 import com.phodal.shirecore.middleware.PostCodeHandleContext
 import com.phodal.shirecore.middleware.PostProcessor
@@ -17,6 +19,14 @@ class OpenFileProcessor : PostProcessor {
     override fun execute(project: Project, context: PostCodeHandleContext, console: ConsoleView?, args: List<Any>): String {
         val file = context.pipeData["output"]
         if (file !is VirtualFile) {
+            // use ide lookup
+            if (file is String) {
+                runInEdt {
+                    FileEditorManager.getInstance(project).openFile(project.lookupFile(file) ?: return@runInEdt)
+                }
+                return ""
+            }
+
             console?.print("No file to open\n", com.intellij.execution.ui.ConsoleViewContentType.ERROR_OUTPUT)
             return ""
         }
@@ -26,4 +36,10 @@ class OpenFileProcessor : PostProcessor {
         }
         return ""
     }
+}
+
+fun Project.lookupFile(path: String): VirtualFile? {
+    val projectPath = this.guessProjectDir()?.toNioPath()
+    val realpath = projectPath?.resolve(path)
+    return VirtualFileManager.getInstance().findFileByUrl("file://${realpath?.toAbsolutePath()}")
 }
