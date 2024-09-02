47efe47a080c4e9fff4cc48a7cbd33acb55bb52a
Phodal Huang <h@phodal.com>
2024-09-02 21:22:51 +0800

    refactor(executor): streamline sh file execution process
    Remove unnecessary dependencies and refactor the ShFile execution process to use a simplified command line execution without the terminal console. This change enhances the performance and readability of the ThreadProcessor by directly capturing the output of the shell command and removing redundant code associated with the previous process handler and console setup.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/ThreadProcessor.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/ThreadProcessor.kt
@@ -1,29 +1,26 @@
 package com.phodal.shirelang.compiler.hobbit.execute
 
-import com.intellij.execution.process.*
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.CapturingProcessHandler
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.NlsContexts.TabTitle
-import com.intellij.openapi.util.Pair
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.readText
-import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.sh.psi.ShFile
 import com.intellij.sh.run.ShRunner
-import com.intellij.terminal.TerminalExecutionConsole
-import com.intellij.terminal.ui.TerminalWidget
-import com.intellij.ui.content.Content
 import com.phodal.shirecore.provider.http.HttpHandler
 import com.phodal.shirecore.provider.http.HttpHandlerType
 import com.phodal.shirecore.provider.shire.FileRunService
 import com.phodal.shirelang.actions.ShireRunFileAction
 import com.phodal.shirelang.compiler.SHIRE_ERROR
 import com.phodal.shirelang.psi.ShireFile
-import com.phodal.shirelang.run.ShireProcessHandler
 import com.phodal.shirelang.utils.lookupFile
+import java.nio.charset.StandardCharsets
 import java.util.concurrent.CompletableFuture
+
 
 object ThreadProcessor {
     fun execute(
@@ -66,7 +63,7 @@ object ThreadProcessor {
                     is Array<*> -> {
                         output.joinToString("\n") {
                             variableTable["output"] = it
-                            executeTask(myProject, variables, variableTable, psi)  ?: "No run service found"
+                            executeTask(myProject, variables, variableTable, psi) ?: "No run service found"
                         }
                     }
 
@@ -75,19 +72,9 @@ object ThreadProcessor {
                     }
                 }
             }
+
             is ShFile -> {
-                val virtualFile = psiFile.virtualFile
-                val workingDirectory = virtualFile.parent.path
-                val shRunner = ApplicationManager.getApplication().getService(ShRunner::class.java)
-                    ?: return "$SHIRE_ERROR: Shell runner not found"
-
-                ApplicationManager.getApplication().invokeAndWait {
-                    if (shRunner.isAvailable(myProject)) {
-                        shRunner.run(myProject, virtualFile.path, workingDirectory, "RunShireShell", true)
-                    }
-                }
-
-                return "Running shell command: $fileName"
+                return executeShFile(psiFile, myProject)
             }
 
             else -> return FileRunService.provider(myProject, file)?.runFile(myProject, file, psiFile)
@@ -95,40 +82,41 @@ object ThreadProcessor {
         }
     }
 
-    private fun executeShFile(
-        psiFile: ShFile,
-        myProject: Project,
-        fileName: String,
-    ): String {
+    private fun executeShFile(psiFile: ShFile, myProject: Project): String {
         val virtualFile = psiFile.virtualFile
-        val workingDirectory = virtualFile.parent.path
         val shRunner = ApplicationManager.getApplication().getService(ShRunner::class.java)
             ?: return "$SHIRE_ERROR: Shell runner not found"
 
         val future = CompletableFuture<String>()
         ApplicationManager.getApplication().invokeAndWait {
             if (shRunner.isAvailable(myProject)) {
-                val processHandler = ShireProcessHandler(fileName)
-                ProcessTerminatedListener.attach(processHandler)
-
-                shRunner.run(myProject, virtualFile.path, workingDirectory, "RunShireShell", false)
-
-                val processAdapter = object : ProcessAdapter() {
-                    override fun processTerminated(event: ProcessEvent) {
-                        future.complete(event.text)
-                    }
-                }
-
-                processHandler.addProcessListener(processAdapter)
-
-                val console: TerminalExecutionConsole = TerminalExecutionConsole(myProject, processHandler)
-                console.attachToProcess(processHandler)
-
-                processHandler.startNotify()
+                val output = runShellCommand(myProject, virtualFile)
+                future.complete(output ?: "No output")
             }
         }
 
         return future.get()
+    }
+
+    private const val DEFAULT_TIMEOUT: Int = 30000
+
+    private fun runShellCommand(project: Project?, virtualFile: VirtualFile): String {
+        val workingDirectory = virtualFile.parent.path
+
+        val commandLine: GeneralCommandLine = GeneralCommandLine()
+            .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+            .withWorkDirectory(workingDirectory)
+            .withCharset(StandardCharsets.UTF_8)
+            .withExePath("sh")
+            .withParameters(virtualFile.path)
+
+        val processOutput = CapturingProcessHandler(commandLine).runProcess(DEFAULT_TIMEOUT)
+        val exitCode = processOutput.exitCode
+        if (exitCode != 0) {
+            throw RuntimeException("Cannot execute ${commandLine}: exit code $exitCode, error output: ${processOutput.stderr}")
+        }
+
+        return processOutput.stdout
     }
 
     private fun executeTask(
