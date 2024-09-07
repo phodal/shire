ce936bda74be3e1aa07bc47ffde133fefa867f8e
JayClock <35857909+JayClock@users.noreply.github.com>
2024-09-07 21:42:24 +0800

    feat(codemodel): Add JavaScript and TypeScript structure providers
    Implements providers for class, file, method, and variable structures.
    These enhancements enable better support for JavaScript and TypeScript
    code analysis within the IDE, allowing for improved refactoring and tooling.
    
    diff:
    - Adds new structure provider classes for JavaScript and TypeScript.
    - Updates plugin configuration to include the new providers.

---
+++ languages/shire-javascript/src/main/kotlin/com/phodal/shirelang/javascript/codemodel/JavaScriptVariableStructureProvider.kt
@@ -0,0 +1,43 @@
+package com.phodal.shirelang.javascript.codemodel
+
+import com.intellij.lang.javascript.psi.JSFieldVariable
+import com.intellij.lang.javascript.psi.JSFunction
+import com.intellij.lang.javascript.psi.util.JSUtils
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.PsiReference
+import com.intellij.psi.util.PsiTreeUtil
+import com.phodal.shirecore.codemodel.VariableStructureProvider
+import com.phodal.shirecore.codemodel.model.VariableStructure
+
+class JavaScriptVariableStructureProvider : VariableStructureProvider {
+    override fun build(
+        psiElement: PsiElement,
+        withMethodContext: Boolean,
+        withClassContext: Boolean,
+        gatherUsages: Boolean
+    ): VariableStructure? {
+        if (psiElement !is JSFieldVariable) {
+            return null
+        }
+
+        val parentOfType: PsiElement? = PsiTreeUtil.getParentOfType(psiElement, JSFunction::class.java, true)
+        val memberContainingClass: PsiElement = JSUtils.getMemberContainingClass(psiElement)
+        val psiReferences: List<PsiReference> = if (gatherUsages) {
+            JavaScriptClassStructureProvider.findUsages(psiElement as PsiNameIdentifierOwner)
+        } else {
+            emptyList()
+        }
+
+        return VariableStructure(
+            psiElement,
+            psiElement.text,
+            psiElement.name!!,
+            parentOfType,
+            memberContainingClass,
+            psiReferences,
+            withMethodContext,
+            withClassContext
+        )
+    }
+}
