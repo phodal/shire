70ef324d15432ab190d25b8ebe8304f65281c5c4
Phodal Huang <h@phodal.com>
2024-09-13 17:53:58 +0800

    feat(marketplace): add MarketplaceToolWindowFactory and ShireIdeaIcons #86
    Added a new MarketplaceToolWindowFactory class to create tool window content for the marketplace. Also, introduced a new ShireIdeaIcons class for loading icons. Updated the shire-main.xml file to include the new tool window and icon.

---
+++ src/main/kotlin/com/phodal/shire/ShireIdeaIcons.kt
@@ -0,0 +1,9 @@
+package com.phodal.shire
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object ShireIdeaIcons {
+    @JvmField
+    val Default: Icon = IconLoader.getIcon("/icons/shire-mkt.svg", ShireIdeaIcons::class.java)
+}
---
+++ src/main/kotlin/com/phodal/shire/marketplace/MarketplaceToolWindowFactory.kt
@@ -0,0 +1,45 @@
+package com.phodal.shire.marketplace
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.StoragePathMacros
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.dsl.builder.panel
+import javax.swing.JPanel
+
+class MarketplaceToolWindowFactory : ToolWindowFactory, DumbAware {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        project.getService(MarketPlaceView::class.java).initToolWindow(toolWindow)
+    }
+
+}
+
+@State(name = "MarketPlaceView", storages = [Storage(StoragePathMacros.PRODUCT_WORKSPACE_FILE)])
+class MarketPlaceView(project: Project) : Disposable {
+    private val myProject = project
+    private var myToolWindowPanel: JPanel? = null
+
+    init {
+        myToolWindowPanel = panel {
+            row {
+                label("Hello, World!")
+            }
+        }
+    }
+
+    fun initToolWindow(toolWindow: ToolWindow) {
+        val contentFactory = ContentFactory.getInstance()
+        val content = contentFactory.createContent(myToolWindowPanel, "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    override fun dispose() {
+        // TODO("Not yet implemented")
+
+    }
+}
