7a4048f3ac39975b165304c74acc26f1ce4548f4
Phodal Huang <h@phodal.com>
2024-09-16 22:01:54 +0800

    feat(httpclient): refactor runFile method and add createConfiguration method
    Refactored the runFile method in HttpClientFileRunService.kt to improve readability and maintainability. The method now uses the readText method from the VirtualFile class instead of manually retrieving the text from the PsiFile. Also, added a new method, createConfiguration, to create a RunConfiguration for a given virtual file.

--- toolsets/httpclient/src/main/kotlin/com/phodal/shire/httpclient/HttpClientFileRunService.kt
+++ toolsets/httpclient/src/main/kotlin/com/phodal/shire/httpclient/HttpClientFileRunService.kt
@@ -4,15 +4,19 @@ import com.intellij.execution.Executor
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.httpClient.http.request.HttpRequestLanguage
+import com.intellij.httpClient.http.request.HttpRequestPsiFile
 import com.intellij.httpClient.http.request.run.HttpRequestExecutorExtensionFactory
 import com.intellij.httpClient.http.request.run.HttpRequestRunConfigurationExecutor
 import com.intellij.httpClient.http.request.run.config.HttpRequestRunConfiguration
 import com.intellij.httpClient.http.request.run.config.HttpRequestRunConfigurationType
 import com.intellij.ide.scratch.ScratchRootType
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.readText
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
@@ -24,44 +28,55 @@ class HttpClientFileRunService : FileRunService {
     }
 
     override fun runFile(project: Project, virtualFile: VirtualFile, psiElement: PsiElement?): String? {
-        val originFile: PsiFile = PsiManager.getInstance(project).findFile(virtualFile) ?: return null
-        val text = originFile.text
+//        val originFile: PsiFile = PsiManager.getInstance(project).findFile(virtualFile) ?: return null
+//        val text = originFile.text
+        val scratchFile = ScratchRootType.getInstance()
+            .createScratchFile(project, "shireRequest.http", HttpRequestLanguage.INSTANCE, virtualFile.readText())
+            ?: return null
 
-        return executeFile(project, text)
+        val runner: RunnerAndConfigurationSettings = runReadAction {
+            val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+                ?: return@runReadAction null
+
+            ConfigurationContext(psiFile)
+                .configurationsFromContext
+                ?.firstOrNull()
+                ?.configurationSettings
+        } ?: return null
+
+        val factory = HttpRequestRunConfigurationType.getInstance().configurationFactories[0]
+        val configuration = HttpRequestRunConfiguration(project, factory, "HttpRequest")
+
+        val runManager: RunManager = RunManager.getInstance(project)
+        configuration.settings.filePath = scratchFile.path
+
+        runManager.setUniqueNameIfNeeded(configuration)
+        runner.isTemporary = true
+        runManager.addConfiguration(runner)
+
+        val selectedRunner = runManager.selectedConfiguration
+        if ((selectedRunner == null || selectedRunner.isTemporary) && runManager.shouldSetRunConfigurationFromContext()) {
+            runManager.selectedConfiguration = runner
+        }
+
+        val executor: Executor = HttpRequestExecutorExtensionFactory.getRunExtension().executor ?: return null
+        HttpRequestRunConfigurationExecutor.getInstance().execute(
+            project, runner, executor
+        )
+
+        return "Run Success"
     }
-}
 
-fun executeFile(project: Project, text: String): String? {
-    val scratchFile = ScratchRootType.getInstance()
-        .createScratchFile(project, "shireRequest.http", HttpRequestLanguage.INSTANCE, text) ?: return null
+    override fun createConfiguration(project: Project, virtualFile: VirtualFile): RunConfiguration? {
+        /// should be HttpRequestPsiFile
+        val psiFile = runReadAction {
+            PsiManager.getInstance(project).findFile(virtualFile) as? HttpRequestPsiFile
+        } ?: return null
 
-    val psiFile = PsiManager.getInstance(project).findFile(scratchFile) ?: return null
+        val factory = HttpRequestRunConfigurationType.getInstance().configurationFactories[0]
+        val configuration = HttpRequestRunConfiguration(project, factory, "HttpRequest")
+        configuration.settings.filePath = virtualFile.path
 
-    val runner: RunnerAndConfigurationSettings = ConfigurationContext(psiFile)
-        .configurationsFromContext
-        ?.firstOrNull()
-        ?.configurationSettings ?: return null
-
-    val factory = HttpRequestRunConfigurationType.getInstance().configurationFactories[0]
-    val configuration = HttpRequestRunConfiguration(project, factory, "HttpRequest")
-
-    val runManager: RunManager = RunManager.getInstance(project)
-
-    configuration.settings.filePath = scratchFile.path
-
-    runManager.setUniqueNameIfNeeded(configuration)
-    runner.isTemporary = true
-    runManager.addConfiguration(runner)
-
-    val selectedRunner = runManager.selectedConfiguration
-    if ((selectedRunner == null || selectedRunner.isTemporary) && runManager.shouldSetRunConfigurationFromContext()) {
-        runManager.selectedConfiguration = runner
+        return configuration
     }
-
-    val executor: Executor = HttpRequestExecutorExtensionFactory.getRunExtension().executor ?: return null
-    HttpRequestRunConfigurationExecutor.getInstance().execute(
-        project, runner, executor
-    )
-
-    return "Run Success"
 }
