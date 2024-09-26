9575b141109b81ad87157bbe2546908960e53868
Phodal Huang <h@phodal.com>
2024-09-26 17:56:34 +0800

    feat(tokenizer): add multiple tokenizer types support in TokenizerProcessor #100
    The commit introduces support for multiple tokenizer types in the TokenizerProcessor class. It now handles regex, word, naming, and stopwords tokenizers. The default tokenizer is set to WordTokenizer.

--- shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/TokenizerProcessor.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/compiler/hobbit/execute/TokenizerProcessor.kt
@@ -1,12 +1,46 @@
 package com.phodal.shirelang.compiler.hobbit.execute
 
 import com.intellij.openapi.project.Project
+import com.phodal.shirecore.search.tokenizer.*
 import com.phodal.shirelang.compiler.patternaction.PatternActionFunc
 
 class TokenizerProcessor {
     companion object {
-        fun execute(myProject: Project, action: PatternActionFunc.Tokenizer): Any {
-            return ""
+        fun execute(project: Project, action: PatternActionFunc.Tokenizer): Any {
+            if (action.tokType.startsWith("/") && action.tokType.endsWith("/")) {
+                val regex = action.tokType.substring(1, action.tokType.length - 1)
+                val tokenizer = RegexpTokenizer(
+                    object : RegexTokenizerOptions {
+                        override val pattern: Regex get() = Regex(regex)
+                        override val discardEmpty: Boolean get() = true
+                        override val gaps: Boolean? get() = false
+                    }
+                )
+
+                return tokenizer.tokenize(action.text)
+            }
+
+            /// if tokType == "word"
+            when (action.tokType) {
+                "word" -> {
+                    val tokenizer = WordTokenizer()
+                    return tokenizer.tokenize(action.text)
+                }
+
+                "naming" -> {
+                    val tokenizer = CodeNamingTokenizer()
+                    return tokenizer.tokenize(action.text)
+                }
+
+                "stopwords" -> {
+                    return StopwordsBasedTokenizer.instance().tokenize(action.text)
+                }
+
+                else -> {
+                    val tokenizer = WordTokenizer()
+                    return tokenizer.tokenize(action.text)
+                }
+            }
         }
     }
 }
