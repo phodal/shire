0cae453f27b9c8b5e7c1f509a387a45e06c745dc
Phodal Huang <h@phodal.com>
2024-10-12 15:50:59 +0800

    refactor(openrewrite): improve run configuration setup process && closed #119
    Extract and simplify the process of setting up and running OpenRewrite configurations, including the retrieval of recipe descriptors and the configuration of working directories and active recipes. This change aims to enhance code readability and maintainability.

--- toolsets/openrewrite/src/main/kotlin/com/phodal/shire/openrewrite/OpenRewriteFileRunService.kt
+++ toolsets/openrewrite/src/main/kotlin/com/phodal/shire/openrewrite/OpenRewriteFileRunService.kt
@@ -3,6 +3,7 @@ package com.phodal.shire.openrewrite
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.ConfigurationTypeUtil
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
@@ -21,10 +22,7 @@ class OpenRewriteFileRunService : FileRunService {
         return isOpenWriteFile(project, file)
     }
 
-    private fun isOpenWriteFile(
-        project: Project,
-        file: VirtualFile,
-    ): Boolean {
+    private fun isOpenWriteFile(project: Project, file: VirtualFile): Boolean {
         try {
             val clazz = Class.forName("com.intellij.openRewrite.OpenRewriteFileService")
             val getInstanceMethod = clazz.getDeclaredMethod("getInstance")
@@ -46,7 +44,7 @@ class OpenRewriteFileRunService : FileRunService {
 
     override fun runConfigurationClass(project: Project): Class<out RunProfile>? = null
 
-    override fun runFile(project: Project, virtualFile: VirtualFile, psiElement: PsiElement?): String {
+    override fun runFile(project: Project, virtualFile: VirtualFile, psiElement: PsiElement?): String? {
         if (!isOpenWriteFile(project, virtualFile)) {
             return ""
         }
@@ -62,55 +60,25 @@ class OpenRewriteFileRunService : FileRunService {
 
             if (configClass.name == "com.intellij.openRewrite.run.OpenRewriteRunConfiguration") {
                 val getExpandedWorkingDirectoryMethod = configClass.getMethod("getExpandedWorkingDirectory")
-                val expandedWorkingDirectory = getExpandedWorkingDirectoryMethod.invoke(config) as? String
-                expandedWorkingDirectory == workingPath
+                val workingDirectory = getExpandedWorkingDirectoryMethod.invoke(config) as? String
+                workingDirectory == workingPath
             } else {
                 false
             }
         }
 
-        // use relfect to get .com.intellij.openRewrite.recipeOpenRewriteRecipeService access getInstance(project)
-        val clazz = Class.forName("com.intellij.openRewrite.recipe.OpenRewriteRecipeService")
-        val getInstanceMethod = clazz.getDeclaredMethod("getInstance", Project::class.java)
-        val openRewriteRecipeService = getInstanceMethod.invoke(null, project)
-        // call getLocalDescriptors in openRewriteRecipeService getLocalDescriptors(psiFile, OpenRewriteType.RECIPE)
-        val getLocalDescriptorsMethod = clazz.getDeclaredMethod("getLocalDescriptors", PsiFile::class.java, OpenRewriteType::class.java)
-        getLocalDescriptorsMethod.isAccessible = true
-
-        val psiFile = runReadAction {
-            PsiManager.getInstance(project).findFile(virtualFile)
-        } ?: return ""
-
-        val list = runReadAction {
-            getLocalDescriptorsMethod.invoke(openRewriteRecipeService, psiFile, OpenRewriteType.RECIPE) as LinkedHashMap<*, *>
-        }
-        if (list.isEmpty()) {
-            return ""
-        }
+        val list = getRecipeDescriptors(project, virtualFile) ?: return null
+        if (list.isEmpty()) return null
 
         if (settings == null) {
-            // ConfigurationType.CONFIGURATION_TYPE_EP.extensionList
             val configurationType = ConfigurationTypeUtil.findConfigurationType("OpenRewriteRunConfigurationType")!!
             settings = runManager.createConfiguration("", configurationType.configurationFactories[0])
             val configuration = settings.configuration
+
             if (configuration.javaClass.name == "com.intellij.openRewrite.run.OpenRewriteRunConfiguration") {
-                val directoryMethod =
-                    configuration::class.java.getMethod("setWorkingDirectory", String::class.java)
-                val projectRoot = project.basePath ?: ""
-                directoryMethod.invoke(configuration, projectRoot)
-
-                val setConfigLocationMethod =
-                    configuration::class.java.getMethod("setConfigLocation", String::class.java)
-                setConfigLocationMethod.invoke(configuration, virtualFile.path)
-
-                // setActiveRecipes(string) from lists.first
-                val setActiveRecipesMethod =
-                    configuration::class.java.getMethod("setActiveRecipes", String::class.java)
-                setActiveRecipesMethod.invoke(configuration, list.keys.first())
-
-                val nameMethod = configuration::class.java.getMethod("setGeneratedName")
-                nameMethod.invoke(configuration)
+                configureOpenRewrite(configuration, project, virtualFile, list)
             }
+
             runManager.setUniqueNameIfNeeded(settings)
             runManager.setTemporaryConfiguration(settings)
         }
@@ -121,5 +89,51 @@ class OpenRewriteFileRunService : FileRunService {
         }
 
         return ""
+    }
+
+    private fun configureOpenRewrite(
+        configuration: RunConfiguration,
+        project: Project,
+        virtualFile: VirtualFile,
+        list: java.util.LinkedHashMap<*, *>,
+    ) {
+        val directoryMethod =
+            configuration::class.java.getMethod("setWorkingDirectory", String::class.java)
+        val projectRoot = project.basePath ?: ""
+        directoryMethod.invoke(configuration, projectRoot)
+
+        val setConfigLocationMethod =
+            configuration::class.java.getMethod("setConfigLocation", String::class.java)
+        setConfigLocationMethod.invoke(configuration, virtualFile.path)
+
+        val setActiveRecipesMethod =
+            configuration::class.java.getMethod("setActiveRecipes", String::class.java)
+        setActiveRecipesMethod.invoke(configuration, list.keys.first())
+
+        val nameMethod = configuration::class.java.getMethod("setGeneratedName")
+        nameMethod.invoke(configuration)
+    }
+
+    private fun getRecipeDescriptors(project: Project, virtualFile: VirtualFile): java.util.LinkedHashMap<*, *>? {
+        val clazz = Class.forName("com.intellij.openRewrite.recipe.OpenRewriteRecipeService")
+        val getInstanceMethod = clazz.getDeclaredMethod("getInstance", Project::class.java)
+        val openRewriteRecipeService = getInstanceMethod.invoke(null, project)
+        val getLocalDescriptorsMethod =
+            clazz.getDeclaredMethod("getLocalDescriptors", PsiFile::class.java, OpenRewriteType::class.java)
+        getLocalDescriptorsMethod.isAccessible = true
+
+        val psiFile = runReadAction {
+            PsiManager.getInstance(project).findFile(virtualFile)
+        } ?: return null
+
+        val list = runReadAction {
+            getLocalDescriptorsMethod.invoke(
+                openRewriteRecipeService,
+                psiFile,
+                OpenRewriteType.RECIPE
+            ) as LinkedHashMap<*, *>
+        }
+
+        return list
     }
 }
