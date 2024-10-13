5b33250ca730e097b81a968eb49271ee80dd922a
Phodal Huang <h@phodal.com>
2024-10-13 15:14:59 +0800

    refactor(shirelang): enhance ShireTypedHandler with new import and logic
    Update ShireTypedHandler by adding necessary imports and refining the charTyped method. This change includes handling the AUTOINSERT_PAIR_BRACKET feature for the '{' character and commiting the document before processing. It also adds a when-statement for conditional logic improvements.

--- shirelang/src/main/kotlin/com/phodal/shirelang/ShireTypedHandler.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/ShireTypedHandler.kt
@@ -2,15 +2,19 @@
 package com.phodal.shirelang
 
 import com.intellij.codeInsight.AutoPopupController
+import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.elementType
 import com.phodal.shirelang.psi.ShireFile
 import com.phodal.shirelang.psi.ShireTypes
+import org.jetbrains.kotlin.utils.addToStdlib.UnsafeCastFunction
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class ShireTypedHandler : TypedHandlerDelegate() {
     override fun checkAutoPopup(charTyped: Char, project: Project, editor: Editor, file: PsiFile): Result {
@@ -46,7 +50,34 @@ class ShireTypedHandler : TypedHandlerDelegate() {
 
     override fun beforeCharTyped(c: Char, project: Project, editor: Editor, file: PsiFile, fileType: FileType): Result {
         if (file.fileType != ShireFileType.INSTANCE) return Result.CONTINUE
-//        if (AppMode.isRemoteDevHost()) return Result.CONTINUE
+
+        return Result.CONTINUE
+    }
+
+
+    @OptIn(UnsafeCastFunction::class)
+    override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
+        if (file.fileType != ShireFileType.INSTANCE) return Result.CONTINUE
+
+        when {
+            c == '{' && CodeInsightSettings.getInstance().AUTOINSERT_PAIR_BRACKET -> {
+                PsiDocumentManager.getInstance(project).commitDocument(editor.document)
+                val offset = editor.caretModel.offset
+                val previousElement = file.findElementAt(offset - 1)
+
+                if (previousElement is LeafPsiElement) {
+                    val identifier = file.findElementAt(offset)
+                        ?.safeAs<LeafPsiElement>()
+                        ?.takeIf { it.elementType == ShireTypes.IDENTIFIER || it.elementType == ShireTypes.QUOTE_STRING }
+                        ?: kotlin.run {
+                            editor.document.insertString(offset, "}")
+                            return Result.STOP
+                        }
+
+                    // todo: add more logic here
+                }
+            }
+        }
 
         return Result.CONTINUE
     }
