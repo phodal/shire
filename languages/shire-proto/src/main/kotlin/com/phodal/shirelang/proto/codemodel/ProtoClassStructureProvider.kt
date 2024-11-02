12ebebbe9ac97a6ee4447f2fa25d9f6356c9854f
Phodal Huang <gmszone@qq.com>
2024-11-02 11:11:06 +0800

    refactor(shire-proto): enhance PsiVariableProvider and ClassStructureProvider #122
    - Update ShireProtoPsiVariableProvider to handle new PsiContextVariable cases.
    - Improve ProtoClassStructureProvider to include fields and usages in class structure.
    - Add method to find usages of a PsiElement in ProtoClassStructureProvider.

--- languages/shire-proto/src/main/kotlin/com/phodal/shirelang/proto/codemodel/ProtoClassStructureProvider.kt
+++ languages/shire-proto/src/main/kotlin/com/phodal/shirelang/proto/codemodel/ProtoClassStructureProvider.kt
@@ -3,7 +3,12 @@ package com.phodal.shirelang.proto.codemodel
 import com.intellij.protobuf.lang.psi.PbDefinition
 import com.intellij.protobuf.lang.psi.PbMessageDefinition
 import com.intellij.protobuf.lang.psi.PbServiceDefinition
+import com.intellij.protobuf.lang.psi.util.PbPsiUtil
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.phodal.shirecore.provider.codemodel.ClassStructureProvider
 import com.phodal.shirecore.provider.codemodel.model.ClassStructure
 
@@ -16,16 +21,24 @@ class ProtoClassStructureProvider : ClassStructureProvider {
                 val text = psiElement.text
                 val name = psiElement.name
 
+                val usages =
+                    if (gatherUsages) findUsages(psiElement as PsiNameIdentifierOwner) else emptyList()
+
+                val fields = mutableListOf<PsiElement>()
+                psiElement.body?.simpleFieldList?.let { fields += it }
+                psiElement.body?.mapFieldList?.let { fields += it }
+                psiElement.body?.oneofDefinitionList?.let { fields += it }
+
                 ClassStructure(
                     psiElement,
                     text,
                     name,
                     name,
                     emptyList(),
-                    emptyList(),
+                    fields,
                     null,
                     emptyList(),
-                    emptyList()
+                    usages
                 )
             }
 
@@ -33,22 +46,37 @@ class ProtoClassStructureProvider : ClassStructureProvider {
                 val text = psiElement.text
                 val name = psiElement.name
 
+                val methods = psiElement.body?.serviceMethodList ?: emptyList()
+
+                val usages =
+                    if (gatherUsages) findUsages(psiElement as PsiNameIdentifierOwner) else emptyList()
+
                 ClassStructure(
                     psiElement,
                     text,
                     name,
                     name,
-                    emptyList(),
+                    methods,
                     emptyList(),
                     null,
                     emptyList(),
-                    emptyList()
+                    usages
                 )
             }
 
             else -> {
                 return null
             }
+        }
+    }
+
+    companion object {
+        fun findUsages(psiElement: PsiElement): List<PsiReference> {
+            val globalSearchScope = GlobalSearchScope.allScope(psiElement.project)
+
+            return ReferencesSearch.search(psiElement, globalSearchScope, true)
+                .findAll()
+                .toList()
         }
     }
 }
--- languages/shire-proto/src/main/kotlin/com/phodal/shirelang/proto/provider/ShireProtoPsiVariableProvider.kt
+++ languages/shire-proto/src/main/kotlin/com/phodal/shirelang/proto/provider/ShireProtoPsiVariableProvider.kt
@@ -3,15 +3,14 @@ package com.phodal.shirelang.proto.provider
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.protobuf.lang.ProtoBaseLanguage
-import com.intellij.protobuf.lang.psi.PbEnumDefinition
-import com.intellij.protobuf.lang.psi.PbFile
-import com.intellij.protobuf.lang.psi.PbMessageDefinition
-import com.intellij.protobuf.lang.psi.PbServiceDefinition
-import com.intellij.protobuf.lang.psi.PbServiceMethod
+import com.intellij.protobuf.lang.psi.*
+import com.intellij.protobuf.lang.psi.util.PbPsiUtil
 import com.intellij.psi.PsiElement
 import com.phodal.shirecore.provider.variable.PsiContextVariableProvider
 import com.phodal.shirecore.provider.variable.impl.CodeSmellBuilder
 import com.phodal.shirecore.provider.variable.model.PsiContextVariable
+import com.phodal.shirelang.proto.codemodel.ProtoClassStructureProvider
+import com.phodal.shirelang.proto.codemodel.ProtoFileStructureProvider
 
 class ShireProtoPsiVariableProvider : PsiContextVariableProvider {
     override fun resolve(variable: PsiContextVariable, project: Project, editor: Editor, psiElement: PsiElement?): Any {
@@ -37,12 +36,14 @@ class ShireProtoPsiVariableProvider : PsiContextVariableProvider {
                     else -> ""
                 }
             }
+
             PsiContextVariable.CURRENT_METHOD_CODE -> return psiElement.text
             PsiContextVariable.RELATED_CLASSES -> TODO()
             PsiContextVariable.SIMILAR_TEST_CASE -> TODO()
             PsiContextVariable.IMPORTS -> {
                 return containingFile.importStatements.joinToString("\n") { it.text }
             }
+
             PsiContextVariable.IS_NEED_CREATE_FILE -> ""
             PsiContextVariable.TARGET_TEST_FILE_NAME -> ""
             PsiContextVariable.UNDER_TEST_METHOD_CODE -> ""
@@ -53,10 +54,25 @@ class ShireProtoPsiVariableProvider : PsiContextVariableProvider {
                 editor
             )
 
-            PsiContextVariable.METHOD_CALLER -> TODO()
-            PsiContextVariable.CALLED_METHOD -> TODO()
-            PsiContextVariable.SIMILAR_CODE -> TODO()
-            PsiContextVariable.STRUCTURE -> TODO()
+            PsiContextVariable.METHOD_CALLER -> ""
+            PsiContextVariable.CALLED_METHOD -> {
+                return when (psiElement) {
+                    is PbServiceDefinition -> {
+//                        PbPsiUtil.resolveRefToType()
+                        ""
+                    }
+                    else -> ""
+                }
+            }
+            PsiContextVariable.SIMILAR_CODE -> ""
+            PsiContextVariable.STRUCTURE -> when (psiElement) {
+                is PbFile -> ProtoFileStructureProvider().build(psiElement)?.toString() ?: ""
+                is PbMessageDefinition,
+                is PbServiceDefinition,
+                    -> ProtoClassStructureProvider().build(psiElement, true)?.toString() ?: ""
+
+                else -> null
+            } ?: ""
         }
     }
 }
