83030fef37a4ca209c00f4da1f5ded887227c7c7
Phodal Huang <gmszone@qq.com>
2024-11-18 21:49:58 +0800

    refactor(core): update ComplexityProvider interface and add ComplexitySink and ComplexityPoint #139
    - Modify ComplexityProvider interface to accept a ComplexitySink in the visitor function.
    - Introduce ComplexitySink class to track and manage complexity points and nesting levels.
    - Add ComplexityPoint data class to represent individual complexity points with their types.

--- core/src/main/kotlin/com/phodal/shirecore/provider/complexity/ComplexityProvider.kt
+++ core/src/main/kotlin/com/phodal/shirecore/provider/complexity/ComplexityProvider.kt
@@ -3,12 +3,13 @@ package com.phodal.shirecore.provider.complexity
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
 import com.intellij.psi.PsiElement
+import com.phodal.shirecore.ast.ComplexitySink
 import com.phodal.shirecore.ast.ComplexityVisitor
 
 interface ComplexityProvider {
     fun process(element: PsiElement): Int
 
-    fun visitor(): ComplexityVisitor
+    fun visitor(sink: ComplexitySink): ComplexityVisitor
 
     companion object {
         private val languageExtension = LanguageExtension<ComplexityProvider>("com.phodal.complexityProvider")
