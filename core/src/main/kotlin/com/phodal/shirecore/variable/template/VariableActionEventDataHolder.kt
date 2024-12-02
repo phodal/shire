07da19511f84aee8a18525deda5a1d3c79aeb857
Phodal Huang <h@phodal.com>
2024-12-02 13:43:35 +0800

    docs(core): enhance VariableActionEventDataHolder comments and usage details
    Updated the comments for the VariableActionEventDataHolder class to provide a clearer description and added usage details. This clarification will help future developers understand the purpose and usage of the class when dealing with VCS variable actions.

--- core/src/main/kotlin/com/phodal/shirecore/variable/template/VariableActionEventDataHolder.kt
+++ core/src/main/kotlin/com/phodal/shirecore/variable/template/VariableActionEventDataHolder.kt
@@ -5,7 +5,27 @@ import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolderBase
 
 /**
- * Temp class to store data context for VCS variable actions
+ * The VariableActionEventDataHolder class serves as a temporary storage for data context related to VCS variable actions.
+ * It uses a data class structure to encapsulate the data and provides a companion object to manage the data storage and retrieval.
+ *
+ * This class is designed to be used with a key-value pair mechanism where the data context is stored against a specific key.
+ * The companion object provides static methods to put and get the data context.
+ *
+ * Usage:
+ *
+ * To store data:
+ * ```Kotlin
+ * val eventData = VariableActionEventDataHolder(dataContext)
+ * VariableActionEventDataHolder.putData(eventData)
+ * ```
+ *
+ * To retrieve data:
+ * ```Kotlin
+ * val eventData = VariableActionEventDataHolder.getData()
+ * ```
+ *
+ * @param dataContext The DataContext object that holds the relevant information for VCS variable actions.
+ *                    It is optional and can be null if no data needs to be stored initially.
  */
 data class VariableActionEventDataHolder(val dataContext: DataContext? = null) {
     companion object {
