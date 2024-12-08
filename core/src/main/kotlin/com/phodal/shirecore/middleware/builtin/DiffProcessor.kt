ea610a4e3febb39a9dd76376e8f6ce5e77027e4a
Phodal Huang <h@phodal.com>
2024-12-08 15:55:33 +0800

    feat(diff): enhance diff view and context handling
    - Add support for diff view in CodeBlockView.
    - Refactor DiffProcessor to use DocumentContent and improve diff request creation.
    - Introduce ShireDiffContext class for better diff context management.

--- core/src/main/kotlin/com/phodal/shirecore/middleware/builtin/DiffProcessor.kt
+++ core/src/main/kotlin/com/phodal/shirecore/middleware/builtin/DiffProcessor.kt
@@ -1,22 +1,29 @@
 package com.phodal.shirecore.middleware.builtin
 
 import com.intellij.diff.DiffContentFactoryEx
+import com.intellij.diff.DiffContext
 import com.intellij.diff.DiffDialogHints
 import com.intellij.diff.DiffManager
 import com.intellij.diff.chains.SimpleDiffRequestChain
 import com.intellij.diff.chains.SimpleDiffRequestProducer
+import com.intellij.diff.contents.DocumentContent
 import com.intellij.diff.requests.SimpleDiffRequest
+import com.intellij.diff.tools.combined.COMBINED_DIFF_MAIN_UI
 import com.intellij.execution.ui.ConsoleView
 import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.UserDataHolder
+import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.util.ui.UIUtil
-import com.phodal.shirecore.middleware.PostProcessorType
-import com.phodal.shirecore.middleware.PostProcessorContext
 import com.phodal.shirecore.middleware.PostProcessor
+import com.phodal.shirecore.middleware.PostProcessorContext
+import com.phodal.shirecore.middleware.PostProcessorType
 
 class DiffProcessor : PostProcessor {
     override val processorName: String = PostProcessorType.Diff.handleName
-    override val description: String = "`diff` will show the diff of two texts, default is current code and llm response"
+    override val description: String =
+        "`diff` will show the diff of two texts, default is current code and llm response"
 
     private val diffFactory = DiffContentFactoryEx.getInstanceEx()
 
@@ -35,20 +42,40 @@ class DiffProcessor : PostProcessor {
             return ""
         }
 
-        val content1 = diffFactory.create(args[0].toString())
-        val content2 = diffFactory.create(args[1].toString())
+        val currentDocContent: DocumentContent = diffFactory.create(args[0].toString())
+        val newDocContent = diffFactory.create(args[1].toString())
 
-
+        val diffRequest =
+            SimpleDiffRequest("Shire Diff", currentDocContent, newDocContent, "Current code", "Llm response")
         val producer = SimpleDiffRequestProducer.create("Shire Diff") {
-            SimpleDiffRequest("Shire Diff", content1, content2, "Current code", "Llm response")
+            diffRequest
         }
 
         val chain = SimpleDiffRequestChain.fromProducer(producer)
         UIUtil.invokeLaterIfNeeded { ->
             DiffManager.getInstance().showDiff(project, chain, DiffDialogHints.DEFAULT)
         }
-
         return ""
     }
+}
 
+class ShireDiffContext(private val project: Project) : DiffContext() {
+    private val mainUi get() = getUserData(COMBINED_DIFF_MAIN_UI)
+
+    private val ownContext: UserDataHolder = UserDataHolderBase()
+
+    override fun getProject() = project
+    override fun isFocusedInWindow(): Boolean = mainUi?.isFocusedInWindow() ?: false
+    override fun isWindowFocused(): Boolean = mainUi?.isWindowFocused() ?: false
+    override fun requestFocusInWindow() {
+        mainUi?.requestFocusInWindow()
+    }
+
+    override fun <T> getUserData(key: Key<T>): T? {
+        return ownContext.getUserData(key)
+    }
+
+    override fun <T> putUserData(key: Key<T>, value: T?) {
+        ownContext.putUserData(key, value)
+    }
 }
