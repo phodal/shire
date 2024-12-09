72a667f856bf8d62d3e67ac9b7fe03fbcbd33545
Phodal Huang <h@phodal.com>
2024-12-09 21:06:20 +0800

    feat(diff): enhance SingleFileDiffLangSketch with new actions and dialog #153
    Update the SingleFileDiffLangSketch to include additional functionalities such as accepting and rejecting patches, and viewing diffs through a new dialog. The commit introduces new imports, refines existing methods, and adds a rollback action with a tooltip for reversing changes. It also modifies the resource bundle to include new messages for the added actions.

--- core/src/main/kotlin/com/phodal/shirecore/ui/viewer/SingleFileDiffLangSketch.kt
+++ core/src/main/kotlin/com/phodal/shirecore/ui/viewer/SingleFileDiffLangSketch.kt
@@ -1,24 +1,40 @@
 package com.phodal.shirecore.ui.viewer
 
+import com.intellij.diff.DiffContentFactory
+import com.intellij.diff.DiffManager
+import com.intellij.diff.contents.DocumentContent
+import com.intellij.diff.requests.SimpleDiffRequest
+import com.intellij.diff.util.DiffUserDataKeys
+import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.icons.AllIcons
 import com.intellij.lang.Language
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diff.impl.patch.PatchReader
 import com.intellij.openapi.diff.impl.patch.TextFilePatch
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.changes.patch.ApplyPatchDefaultExecutor
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.vcs.VcsApplicationSettings
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.openapi.vcs.changes.patch.*
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.LightVirtualFile
+import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.containers.MultiMap
 import com.intellij.util.ui.JBUI
 import com.phodal.shirecore.ShireCoreBundle
 import com.phodal.shirecore.ShirelangNotifications
 import com.phodal.shirecore.findFile
 import java.awt.BorderLayout
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.*
@@ -51,8 +67,8 @@ class DiffLangSketch(private val myProject: Project, private var patchContent: S
             filePatches
                 .filter { it.beforeFileName != null }
                 .forEach { patch ->
-                    val file = myProject.findFile(patch.beforeFileName!!) ?: return@forEach
-                    val diffPanel = SingleFileDiffLangSketch(myProject, file).getComponent()
+                    val originFile = myProject.findFile(patch.beforeFileName!!) ?: return@forEach
+                    val diffPanel = SingleFileDiffLangSketch(myProject, originFile).getComponent()
                     mainPanel.add(diffPanel)
                 }
         }
@@ -71,7 +87,7 @@ class DiffLangSketch(private val myProject: Project, private var patchContent: S
             icon = AllIcons.Actions.Rollback
             toolTipText = ShireCoreBundle.message("sketch.patch.action.reject.tooltip")
             addActionListener {
-//                handleRejectAction()
+                handleRejectAction()
             }
         }
 
@@ -95,12 +111,33 @@ class DiffLangSketch(private val myProject: Project, private var patchContent: S
     }
 
     private fun handleAcceptAction() {
+        ApplicationManager.getApplication().invokeAndWait {
+            val matchedPatches =
+                MatchPatchPaths(myProject).execute(filePatches, true)
 
+            val patchGroups = MultiMap<VirtualFile, AbstractFilePatchInProgress<*>>()
+            for (patchInProgress in matchedPatches) {
+                patchGroups.putValue(patchInProgress.base, patchInProgress)
+            }
+
+            if(filePatches.isEmpty() ) {
+                ShirelangNotifications.error(myProject, "PatchProcessor: no patches found")
+                return@invokeAndWait
+            }
+
+            val additionalInfo = myReader.getAdditionalInfo(ApplyPatchDefaultExecutor.pathsFromGroups(patchGroups))
+            shelfExecutor.apply(filePatches, patchGroups, null, "LlmGen.diff", additionalInfo)
+        }
+    }
+
+    private fun handleRejectAction() {
+        //
     }
 
     private fun handleViewDiffAction() {
-
+        MyApplyPatchFromClipboardDialog(myProject, patchContent).show()
     }
+
 
     override fun getViewText(): String = patchContent
     override fun updateViewText(text: String) {
@@ -116,6 +153,8 @@ class DiffLangSketch(private val myProject: Project, private var patchContent: S
 class SingleFileDiffLangSketch(private val myProject: Project, private val filepath: VirtualFile) {
     private val mainPanel: JPanel = JPanel(VerticalLayout(5))
     private val myHeaderPanel: JPanel = JPanel(BorderLayout())
+
+    private val originContent = String(filepath.contentsToByteArray())
 
     init {
         val contentPanel = JPanel(BorderLayout())
@@ -140,13 +179,15 @@ class SingleFileDiffLangSketch(private val myProject: Project, private val filep
             })
         }
 
-        val actions = JLabel(AllIcons.Actions.Rollback)
+        val actions = createActions()
 
         val filePanel = panel {
             row {
                 cell(fileIcon).align(AlignX.LEFT)
                 cell(filepathComponent).align(AlignX.LEFT)
-                cell(actions).align(AlignX.RIGHT)
+                actions.forEach {
+                    cell(it).align(AlignX.RIGHT)
+                }
             }
         }.also {
             it.background = JBColor(0xF5F5F5, 0x333333)
@@ -159,7 +200,46 @@ class SingleFileDiffLangSketch(private val myProject: Project, private val filep
         mainPanel.add(contentPanel)
     }
 
+    private fun createActions(): List<JComponent> {
+        val rollback = JLabel(AllIcons.Actions.Rollback).apply {
+            toolTipText = ShireCoreBundle.message("sketch.patch.action.rollback.tooltip")
+            addMouseListener(object : MouseAdapter() {
+                override fun mouseClicked(e: MouseEvent?) {
+                    runWriteAction {
+                        filepath.setBinaryContent(originContent.toByteArray())
+                    }
+                }
+            })
+        }
+
+        return listOf(rollback)
+    }
+
     fun getComponent(): JComponent {
         return mainPanel
+    }
+}
+
+class MyApplyPatchFromClipboardDialog(project: Project, clipboardText: String) :
+    ApplyPatchDifferentiatedDialog(
+        project, ApplyPatchDefaultExecutor(project), emptyList(), ApplyPatchMode.APPLY_PATCH_IN_MEMORY,
+        LightVirtualFile("clipboardPatchFile", clipboardText), null, null,  //NON-NLS
+        null, null, null, false
+    ) {
+    override fun createDoNotAskCheckbox(): JComponent? {
+        return createAnalyzeOnTheFlyOptionPanel()
+    }
+
+    companion object {
+        private fun createAnalyzeOnTheFlyOptionPanel(): JCheckBox {
+            val removeOptionCheckBox =
+                JCheckBox(VcsBundle.message("patch.apply.analyze.from.clipboard.on.the.fly.checkbox"))
+            removeOptionCheckBox.mnemonic = KeyEvent.VK_L
+            removeOptionCheckBox.isSelected = VcsApplicationSettings.getInstance().DETECT_PATCH_ON_THE_FLY
+            removeOptionCheckBox.addActionListener { e: ActionEvent? ->
+                VcsApplicationSettings.getInstance().DETECT_PATCH_ON_THE_FLY = removeOptionCheckBox.isSelected
+            }
+            return removeOptionCheckBox
+        }
     }
 }
