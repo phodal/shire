17eb9ffbdd7960fb5d29b97a80f29d352127dbf8
Phodal Huang <h@phodal.com>
2024-12-11 20:11:20 +0800

    feat(ui): add patch display and action buttons to ShireMarketplaceTableView #153
    Adds the functionality to display patch content and associated action buttons within the ShireMarketplaceTableView. This includes a view for the patch content and the implementation of action buttons for file diff operations, such as rollback, enhancing the user interface for managing code patches.

--- core/src/main/kotlin/com/phodal/shirecore/sketch/patch/SingleFileDiffView.kt
+++ core/src/main/kotlin/com/phodal/shirecore/sketch/patch/SingleFileDiffView.kt
@@ -13,28 +13,30 @@ import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.RightGap
 import com.intellij.ui.dsl.builder.panel
 import com.phodal.shirecore.ShireCoreBundle
 import com.phodal.shirecore.sketch.LangSketch
 import java.awt.BorderLayout
-import java.awt.Color
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import javax.swing.*
+import javax.swing.BorderFactory
+import javax.swing.JButton
+import javax.swing.JComponent
+import javax.swing.JPanel
 
 
 class SingleFileDiffView(private val myProject: Project, private val virtualFile: VirtualFile) : LangSketch {
     private val mainPanel: JPanel = JPanel(VerticalLayout(5))
     private val myHeaderPanel: JPanel = JPanel(BorderLayout())
+    private var filePanel: DialogPanel? = null
 
     init {
         val contentPanel = JPanel(BorderLayout())
-
+        val actions = createActionButtons()
         val filepathLabel = JBLabel(virtualFile.name).apply {
             icon = virtualFile.fileType.icon
-            background = JBColor(Color(200, 216, 230), Color(135, 206, 235))
-            isOpaque = true
-            border = BorderFactory.createEmptyBorder(5, 10, 5, 10)
+            border = BorderFactory.createEmptyBorder(2, 10, 2, 10)
 
             addMouseListener(object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
@@ -42,24 +44,31 @@ class SingleFileDiffView(private val myProject: Project, private val virtualFile
                 }
 
                 override fun mouseEntered(e: MouseEvent) {
-                    foreground = JBColor(Color.WHITE, Color.WHITE)
-                    background = JBColor(DarculaColors.BLUE, DarculaColors.BLUE)
+                    foreground = JBColor.WHITE
+                    actions.forEach { it.foreground = JBColor.WHITE }
+                    filePanel?.background = JBColor(DarculaColors.BLUE, DarculaColors.BLUE)
                 }
 
                 override fun mouseExited(e: MouseEvent) {
-                    background = JBColor(Color(200, 216, 230), Color(135, 206, 235))
+                    foreground = JBColor.BLACK
+                    actions.forEach { it.foreground = JBColor.BLACK }
+                    filePanel?.background = JBColor.PanelBackground
                 }
             })
         }
 
-        val actions = createActions()
-        val filePanel: DialogPanel = panel {
+        filePanel = panel {
             row {
                 cell(filepathLabel).align(AlignX.FILL).resizableColumn()
-                actions.forEach {
-                    cell(it).align(AlignX.LEFT)
+                actions.forEachIndexed { index, action ->
+                    cell(action).align(AlignX.LEFT)
+                    if (index < actions.size - 1) {
+                        this@panel.gap(RightGap.SMALL)
+                    }
                 }
             }
+        }.apply {
+            background = JBColor.PanelBackground
         }
 
         val fileContainer = JPanel(BorderLayout(10, 10)).also {
@@ -71,14 +80,16 @@ class SingleFileDiffView(private val myProject: Project, private val virtualFile
         mainPanel.add(contentPanel)
     }
 
-    private fun createActions(): List<JComponent> {
+    private fun createActionButtons(): List<JButton> {
         val undoManager = UndoManager.getInstance(myProject)
         val fileEditor = FileEditorManager.getInstance(myProject).getSelectedEditor(virtualFile)
 
         val rollback = JButton(AllIcons.Actions.Rollback).apply {
             toolTipText = ShireCoreBundle.message("sketch.patch.action.rollback.tooltip")
-            isOpaque = true
             isEnabled = undoManager.isUndoAvailable(fileEditor)
+            border = null
+            isFocusPainted = false
+            isContentAreaFilled = false
 
             addMouseListener(object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
