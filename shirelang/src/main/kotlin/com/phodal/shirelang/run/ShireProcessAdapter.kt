197428209a15432c2ea7af5fca2639a0d7dd156e
Phodal Huang <h@phodal.com>
2024-12-22 22:05:22 +0800

    docs(shirelang): add comments to ShireProcessAdapter
    - Add class-level comment explaining the adapter's purpose.
    - Add method-level comment for `setLlmOutput`.

--- shirelang/src/main/kotlin/com/phodal/shirelang/run/ShireProcessAdapter.kt
+++ shirelang/src/main/kotlin/com/phodal/shirelang/run/ShireProcessAdapter.kt
@@ -5,6 +5,10 @@ import com.intellij.execution.process.ProcessEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.Key
 
+/**
+ * Adapter for the process of the Shire run configuration.
+ * Will be used to get the result of the process and to notify the listeners when the process is terminated.
+ */
 class ShireProcessAdapter(val configuration: ShireConfiguration, val consoleView: ShireConsoleView?) :
     ProcessAdapter() {
     var result = ""
@@ -12,7 +16,6 @@ class ShireProcessAdapter(val configuration: ShireConfiguration, val consoleView
 
     override fun processTerminated(event: ProcessEvent) {
         super.processTerminated(event)
-
         ApplicationManager.getApplication().messageBus
             .syncPublisher(ShireRunListener.TOPIC)
             .runFinish(result, llmOutput, event, configuration.getScriptPath(), consoleView)
@@ -23,6 +26,9 @@ class ShireProcessAdapter(val configuration: ShireConfiguration, val consoleView
         result = consoleView?.output().toString()
     }
 
+    /**
+     * When the process is terminated, will use the given llmOutput to set the llmOutput of the adapter.
+     */
     fun setLlmOutput(llmOutput: String?) {
         if (llmOutput != null) {
             this.llmOutput = llmOutput
