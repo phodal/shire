cdfe6371f568e6f391d9ec9ce54f3a38ebdb61cd
Phodal Huang <h@phodal.com>
2024-12-31 12:57:36 +0800

    refactor(database): streamline SQL execution logic #171
    Extracted SQL execution logic into separate functions (`executeSqlInConsole` and `executeSql`) for better readability and maintainability. Removed unused imports and redundant code. Simplified the flow of SQL execution and error handling.

--- toolsets/database/src/main/kotlin/com/phodal/shire/database/DatabaseSchemaAssistant.kt
+++ toolsets/database/src/main/kotlin/com/phodal/shire/database/DatabaseSchemaAssistant.kt
@@ -2,21 +2,11 @@ package com.phodal.shire.database
 
 import com.intellij.database.console.DatabaseRunners
 import com.intellij.database.console.JdbcConsole
-import com.intellij.database.console.JdbcConsoleBase
 import com.intellij.database.console.JdbcConsoleProvider
 import com.intellij.database.console.evaluation.EvaluationRequest
-import com.intellij.database.console.session.DatabaseSession
 import com.intellij.database.console.session.DatabaseSessionManager
-import com.intellij.database.console.session.DatabaseSessionManagerListener
-import com.intellij.database.dataSource.DatabaseConnectionCore
-import com.intellij.database.dataSource.InterruptibleDatabaseConnection
-import com.intellij.database.dataSource.connection.DGDepartment
-import com.intellij.database.dataSource.connection.statements.Configuration
-import com.intellij.database.dataSource.connection.statements.ReusableSmartStatement
-import com.intellij.database.dataSource.connection.statements.SmartStatements
 import com.intellij.database.datagrid.*
 import com.intellij.database.editor.DatabaseEditorHelper
-import com.intellij.database.editor.DatabaseEditorHelperCore
 import com.intellij.database.intentions.RunQueryInConsoleIntentionAction.Manager.chooseAndRunRunners
 import com.intellij.database.model.DasTable
 import com.intellij.database.model.ObjectKind
@@ -26,14 +16,11 @@ import com.intellij.database.psi.DbPsiFacade
 import com.intellij.database.script.PersistenceConsoleProvider
 import com.intellij.database.settings.DatabaseSettings
 import com.intellij.database.util.DasUtil
-import com.intellij.database.util.DbImplUtil
 import com.intellij.database.util.DbImplUtilCore
-import com.intellij.database.util.ErrorHandler
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.sql.psi.SqlPsiFacade
 import com.intellij.testFramework.LightVirtualFile
@@ -84,36 +71,13 @@ object DatabaseSchemaAssistant {
             ?: createConsole(project, file)
 
         if (console != null) {
-            val future: CompletableFuture<String> = CompletableFuture()
-            return ApplicationManager.getApplication().executeOnPooledThread<String> {
-                val messageBus = console.session.messageBus
-                val newConsoleRequest = EvaluationRequest.newRequest(console, sql, dataSource.dbms)
-                messageBus.dataProducer.processRequest(newConsoleRequest)
-                messageBus.addConsumer(object : DataConsumer {
-                    var result = mutableListOf<GridRow>()
-                    override fun setColumns(context: GridDataRequest.Context, columns: Array<out GridColumn>) {
-                        super.setColumns(context, columns)
-                    }
-
-                    override fun addRows(context: GridDataRequest.Context, rows: MutableList<out GridRow>) {
-                        super.addRows(context, rows)
-                        result += rows
-                        /// TODO: fix this use result.size instead of rows.size
-                        if (rows.size < 100) {
-                            future.complete(result.toString())
-                        }
-                    }
-                })
-
-                return@executeOnPooledThread future.get()
-            }.get()
+            return executeSqlInConsole(console, sql, dataSource)
         }
 
         val editor = FileEditorManager.getInstance(project).selectedTextEditor
             ?: throw IllegalArgumentException("ShireError[Database]: No editor found")
 
-        val scriptModel = console?.scriptModel
-            ?: SqlPsiFacade.getInstance(project).createScriptModel(psiFile)
+        val scriptModel = SqlPsiFacade.getInstance(project).createScriptModel(psiFile)
 
         val dasNamespace = dataSource.model.currentRootNamespace
         DatabaseEditorHelper.openConsoleForFile(project, dataSource, dasNamespace, file)
@@ -121,26 +85,45 @@ object DatabaseSchemaAssistant {
         val info = JdbcConsoleProvider.Info(psiFile, psiFile, editor as EditorEx, scriptModel, execOptions, null)
         val runners: MutableList<PersistenceConsoleProvider.Runner> = DatabaseRunners.getAttachDataSourceRunners(info)
         if (runners.size == 1) {
-            val runner = runners.first()
             var result = ""
             ApplicationManager.getApplication().invokeAndWait {
-                result = runSingleRunner(psiFile, project, dataSource, runner, sql) ?: "Error"
+                result = executeSql(project, dataSource, sql) ?: "Error"
             }
+
             return result
         } else {
             chooseAndRunRunners(runners, info.editor, null)
+            return "ShireError[Database]: Currently not support multiple runners"
         }
-
-        return "Error"
     }
 
-    private fun runSingleRunner(
-        psiFile: PsiFile,
-        project: Project,
-        dataSource: RawDataSource,
-        runner: PersistenceConsoleProvider.Runner,
-        query: String,
-    ): String? {
+    private fun executeSqlInConsole(console: JdbcConsole, sql: String, dataSource: RawDataSource): String {
+        val future: CompletableFuture<String> = CompletableFuture()
+        return ApplicationManager.getApplication().executeOnPooledThread<String> {
+            val messageBus = console.session.messageBus
+            val newConsoleRequest = EvaluationRequest.newRequest(console, sql, dataSource.dbms)
+            messageBus.dataProducer.processRequest(newConsoleRequest)
+            messageBus.addConsumer(object : DataConsumer {
+                var result = mutableListOf<GridRow>()
+                override fun setColumns(context: GridDataRequest.Context, columns: Array<out GridColumn>) {
+                    super.setColumns(context, columns)
+                }
+
+                override fun addRows(context: GridDataRequest.Context, rows: MutableList<out GridRow>) {
+                    super.addRows(context, rows)
+                    result += rows
+                    /// TODO: fix this use result.size instead of rows.size
+                    if (rows.size < 100) {
+                        future.complete(result.toString())
+                    }
+                }
+            })
+
+            return@executeOnPooledThread future.get()
+        }.get()
+    }
+
+    private fun executeSql(project: Project, dataSource: RawDataSource, query: String): String? {
         val future: CompletableFuture<String> = CompletableFuture()
         val localDs = DbImplUtilCore.getLocalDataSource(dataSource)
 
@@ -155,7 +138,6 @@ object DatabaseSchemaAssistant {
             override fun addRows(context: GridDataRequest.Context, rows: MutableList<out GridRow>) {
                 super.addRows(context, rows)
                 result += rows
-                println("rows")
                 if (rows.size < 100) {
                     future.complete(result.toString())
                 }
@@ -166,51 +148,6 @@ object DatabaseSchemaAssistant {
             object : DataRequest.QueryRequest(session, query, DataRequest.newConstraints(dataSource.dbms), null) {}
         messageBus.dataProducer.processRequest(request)
         return future.get()
-    }
-
-    private fun runSingleRunner2(
-        psiFile: PsiFile,
-        project: Project,
-        dataSource: RawDataSource,
-        runner: PersistenceConsoleProvider.Runner,
-    ) {
-        val handler = ErrorHandler()
-        //            val future: CompletableFuture<String> = CompletableFuture()
-        //            return ApplicationManager.getApplication().executeOnPooledThread<String> {
-        val localDs = DbImplUtilCore.getLocalDataSource(dataSource)
-//        val namespace = localDs.model.currentRootNamespace
-//        val searchPath = DatabaseEditorHelperCore.getSearchPath(psiFile)
-        val searchPath = DatabaseEditorHelperCore.getSearchPath(psiFile)
-
-        val facade = DatabaseSessionManager
-            .getFacade(
-                project, localDs, null, searchPath, false, handler, DGDepartment.DATA_EXPORT
-            )
-
-        val msgConnection = project.messageBus.connect()
-        msgConnection.subscribe(DatabaseSessionManager.topic, object : DatabaseSessionManagerListener {
-            override fun sessionOpened(session: DatabaseSession) {
-                ///
-            }
-
-            override fun sessionClosed(session: DatabaseSession) {
-                println("Session closed")
-            }
-        })
-
-        facade.runSync { connection: InterruptibleDatabaseConnection ->
-            var statement: ReusableSmartStatement<String>? = null
-            try {
-                statement = SmartStatements.poweredBy(connection).simple(Configuration.default).reuse()
-                runner.run()
-            } finally {
-                //                            future.complete("Done")
-                statement?.close()
-            }
-        }
-
-        //                return@executeOnPooledThread future.get()
-        //            }.get()
     }
 
     private fun createConsole(project: Project, file: LightVirtualFile): JdbcConsole? {
