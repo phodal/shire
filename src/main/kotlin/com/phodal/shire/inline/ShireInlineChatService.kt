a9cfd60982a105c0995bb6f1cff73f6005e2b837
Phodal Huang <h@phodal.com>
2025-01-01 11:04:46 +0800

    feat(inline-chat): enhance inline chat panel with improved UI and state management #157
    - Added `ConcurrentHashMap` to manage multiple chat panels and their states.
    - Improved UI with rounded borders, background colors, and better layout handling.
    - Implemented close functionality for chat panels on ESC key press.
    - Enhanced panel resizing and redrawing logic for better user experience.

--- src/main/kotlin/com/phodal/shire/inline/ShireInlineChatService.kt
+++ src/main/kotlin/com/phodal/shire/inline/ShireInlineChatService.kt
@@ -2,43 +2,77 @@ package com.phodal.shire.inline
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorCustomElementRenderer
 import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.ui.Gray
+import com.intellij.ui.JBColor
+import com.intellij.ui.RoundedLineBorder
 import com.intellij.ui.components.JBTextArea
-import com.intellij.util.ui.JBUI
 import java.awt.*
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import java.awt.geom.Rectangle2D
-import javax.swing.BorderFactory
-import javax.swing.JPanel
-import javax.swing.LayoutFocusTraversalPolicy
+import java.util.concurrent.ConcurrentHashMap
+import javax.swing.*
+import javax.swing.border.Border
 
 @Service(Service.Level.APP)
 class ShireInlineChatService : Disposable {
-    fun showInlineChat(editor: Editor) {
-        runInEdt {
-            if (editor.component !is ShireInlineChatPanel) {
-                val panel = ShireInlineChatPanel(editor)
-                editor.contentComponent.add(panel)
-                panel.setInlineContainer(editor.contentComponent)
-                val offset = if (editor.selectionModel.hasSelection()) {
-                    editor.selectionModel.selectionStart
-                } else {
-                    editor.caretModel.primaryCaret.offset
-                }
+    private val allChats: ConcurrentHashMap<String, ShireInlineChatPanel> = ConcurrentHashMap()
 
-                panel.createInlay(offset)
-                IdeFocusManager.getInstance(editor.project).requestFocus(panel.inputPanel.getInputComponent(), true)
+    fun showInlineChat(editor: Editor) {
+        var canShowInlineChat = true
+        if (allChats.containsKey(editor.virtualFile.url)) {
+            val chatPanel: ShireInlineChatPanel = this.allChats[editor.virtualFile.url]!!
+            canShowInlineChat = chatPanel.inlay?.offset != editor.caretModel.primaryCaret.offset
+            closeInlineChat(editor)
+        }
+
+        if (canShowInlineChat) {
+            if (editor.component is ShireInlineChatPanel) return
+
+            val panel = ShireInlineChatPanel(editor)
+            editor.contentComponent.add(panel)
+            panel.setInlineContainer(editor.contentComponent)
+
+            val offset = if (editor.selectionModel.hasSelection()) {
+                editor.selectionModel.selectionStart
+            } else {
+                editor.caretModel.primaryCaret.offset
             }
+            panel.createInlay(offset)
+
+            IdeFocusManager.getInstance(editor.project).requestFocus(panel.inputPanel.getInputComponent(), true)
+            allChats[editor.virtualFile.url] = panel
         }
     }
 
     override fun dispose() {
-        ///
+        allChats.values.forEach {
+            closeInlineChat(it.editor)
+        }
+
+        allChats.clear()
+    }
+
+    fun closeInlineChat(editor: Editor) {
+        val chatPanel = this.allChats[editor.virtualFile.url] ?: return
+
+        chatPanel.inlay?.dispose()
+        chatPanel.inlay = null
+
+        // clear sessions for chat in here
+
+        editor.contentComponent.remove(chatPanel)
+        editor.contentComponent.revalidate()
+        editor.contentComponent.repaint()
+        allChats.remove(editor.virtualFile.url)
     }
 
     companion object {
@@ -48,46 +82,63 @@ class ShireInlineChatService : Disposable {
     }
 }
 
-class ShireInlineChatPanel(private val editor: Editor) : JPanel(GridBagLayout()), EditorCustomElementRenderer,
+class ShireInlineChatPanel(val editor: Editor) : JPanel(GridBagLayout()), EditorCustomElementRenderer,
     Disposable {
-    private var inlay: Inlay<*>? = null
-    val inputPanel = ShireInlineChatInputPanel()
+    var inlay: Inlay<*>? = null
+    val inputPanel = ShireInlineChatInputPanel(this)
     private var centerPanel: JPanel = JPanel(BorderLayout())
     private var container: Container? = null
 
     init {
-        border = JBUI.Borders.empty()
+        val createEmptyBorder = BorderFactory.createEmptyBorder(12, 12, 12, 12)
+        val roundedLineBorder: Border = RoundedLineBorder(JBColor(Gray.xCD, Gray.x4D), 8, 1)
+        border = BorderFactory.createCompoundBorder(createEmptyBorder, roundedLineBorder)
+
         isOpaque = false
         cursor = Cursor.getPredefinedCursor(0)
+        background = JBColor(Gray.x99, Gray.x78)
+
+        val c = GridBagConstraints()
+        c.gridx = 0
+        c.gridy = 0
+        c.weightx = 1.0
+        c.fill = 2
+        add(inputPanel, c)
 
         val jPanel = JPanel(BorderLayout())
-        jPanel.addMouseListener(object : java.awt.event.MouseAdapter() {
-            override fun mouseClicked(e: java.awt.event.MouseEvent) {
+        jPanel.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
                 IdeFocusManager.getInstance(editor.project).requestFocus(inputPanel.getInputComponent(), true)
             }
         })
         this.centerPanel = jPanel
 
-        val c = GridBagConstraints()
         c.gridx = 0
         c.gridy = 1
-        c.weightx = 1.0
-        c.anchor = GridBagConstraints.NORTHWEST
-        c.fill = GridBagConstraints.HORIZONTAL
+        c.fill = 1
         add(this.centerPanel, c)
-
-        c.gridy = 0
-        c.weighty = 0.0
-        c.fill = GridBagConstraints.HORIZONTAL
-        add(inputPanel, c)
 
         isFocusCycleRoot = true
         focusTraversalPolicy = LayoutFocusTraversalPolicy()
+
+        redraw()
     }
 
-    override fun calcWidthInPixels(p0: Inlay<*>): Int = size.width
+    override fun calcWidthInPixels(inlay: Inlay<*>): Int = size.width
 
-    override fun calcHeightInPixels(p0: Inlay<*>): Int = size.height
+    override fun calcHeightInPixels(inlay: Inlay<*>): Int = size.height
+
+    private fun redraw() {
+        ApplicationManager.getApplication().invokeLater {
+            if (this.size.height != this.getMinimumSize().height) {
+                this.size = Dimension(800, this.getMinimumSize().height)
+                this.inlay?.update()
+
+                this.revalidate()
+                this.repaint()
+            }
+        }
+    }
 
     fun createInlay(offset: Int) {
         inlay = editor.inlayModel.addBlockElement(offset, false, true, 1, this)
@@ -108,23 +159,29 @@ class ShireInlineChatPanel(private val editor: Editor) : JPanel(GridBagLayout())
     }
 }
 
-class ShireInlineChatInputPanel : JPanel(GridBagLayout()) {
+class ShireInlineChatInputPanel(val shireInlineChatPanel: ShireInlineChatPanel) : JPanel(GridBagLayout()) {
     private val textArea: JBTextArea
 
     init {
         layout = BorderLayout()
         textArea = JBTextArea().apply {
+            val closeOnEscapeAction = object : AbstractAction() {
+                override fun actionPerformed(e: ActionEvent) {
+                    ShireInlineChatService.getInstance().closeInlineChat(shireInlineChatPanel.editor)
+                }
+            }
+
             isOpaque = false
             isFocusable = true
             lineWrap = true
             wrapStyleWord = true
             border = BorderFactory.createEmptyBorder(8, 5, 8, 5)
-        }
+            actionMap.put("closeOnEscape", closeOnEscapeAction)
+            inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "closeOnEscape")
+         }
 
         add(textArea)
     }
 
-    fun getInputComponent(): JBTextArea {
-        return textArea
-    }
+    fun getInputComponent(): Component = textArea
 }
